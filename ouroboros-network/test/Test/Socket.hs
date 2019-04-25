IntersectMBO/ouroboros-network

Added withNetworkNode
Run a network node within in a bracket.  This is a convenient way of
running a node which will take care of the resouces (socket,
thread in which server runs) either in normal or in error condition.

ouroboros-network/test/Test/Socket.hs
@@ -158,14 +158,11 @@ prop_socket_send_recv clientAddr serverAddr clientVersions serverVersions f xs =
              protocols     = \_ -> Just cliPeers
            }
 
-    serNode <- runNetworkNodeWithSocket serNet
-    cliNode <- runNetworkNodeWithSocket cliNet
-
-    res <- withConnection cliNode serverAddr $ \_ ->
-      atomically $ (,) <$> takeTMVar sv <*> takeTMVar cv
-
-    killNode cliNode
-    killNode serNode
+    res <-
+      withNetworkNode serNet $ \_ ->
+        withNetworkNode cliNet $ \cliNode ->
+          withConnection cliNode serverAddr $ \_ ->
+            atomically $ (,) <$> takeTMVar sv <*> takeTMVar cv
 
     return (res == mapAccumL f 0 xs)
 
@@ -233,17 +230,12 @@ prop_socket_client_connect_error _ xs = ioProperty $ do
             protocols     = \_ -> Just cliPeers
           }
 
-    nn <- runNetworkNodeWithSocket ni
-    (conn_e :: Either SomeException (Connection IO))
-       <- try $ connectTo nn clientAddr
-    case conn_e of
-      Left _  -> do
-        killNode nn
-        return $ property True -- XXX Disregarding the exact exception type 
-      Right conn -> do
-        terminate conn
-        killNode nn
-        return $ property False
+    (res :: Either SomeException Bool)
+      <- try $ withNetworkNode ni $ \nn ->
+                 withConnection nn clientAddr $ \_ -> pure False
+
+    -- XXX Disregarding the exact exception type 
+    pure $ either (const True) id res
 
 
 prop_version_missmatch :: (Int -> Int -> (Int, Int))
@@ -397,27 +389,20 @@ demo chain0 updates = do
               protocols     = \_ -> Just producerPeers
             }
 
+    withNetworkNode producerNet $ \_ ->
+      withNetworkNode consumerNet $  \consumerNode ->
+        withConnection consumerNode (nodeAddress producerNet) $ \_ -> do
+          void $ fork $ sequence_
+              [ do
+                  threadDelay 10000 -- just to provide interest
+                  atomically $ do
+                    p <- readTVar producerVar
+                    let Just p' = CPS.applyChainUpdate update p
+                    writeTVar producerVar p'
+              | update <- updates
+              ]
 
-    producerNode <- runNetworkNodeWithSocket producerNet
-    consumerNode <- runNetworkNodeWithSocket consumerNet
-
-    r <- withConnection consumerNode (nodeAddress producerNet) $ \_ -> do
-
-      void $ fork $ sequence_
-          [ do threadDelay 10000 -- just to provide interest
-               atomically $ do
-                  p <- readTVar producerVar
-                  let Just p' = CPS.applyChainUpdate update p
-                  writeTVar producerVar p'
-          | update <- updates
-          ]
-
-      atomically $ takeTMVar done
-
-    killNode producerNode
-    killNode consumerNode
-
-    return r
+          atomically $ takeTMVar done
   where
     checkTip target consumerVar = atomically $ do
       chain <- readTVar consumerVar

IntersectMBO/cardano-world

ouroboros-network/test/Test/Socket.hs
@@ -135,14 +135,11 @@ prop_socket_send_recv clientAddr serverAddr f xs = do
              protocols   = cliPeers
            }
 
-    serNode <- runNetworkNodeWithSocket serNet
-    cliNode <- runNetworkNodeWithSocket cliNet
-
-    res <- withConnection cliNode serverAddr $ \_ ->
-      atomically $ (,) <$> takeTMVar sv <*> takeTMVar cv
-
-    killNode cliNode
-    killNode serNode
+    res <-
+      withNetworkNode serNet $ \_ ->
+        withNetworkNode cliNet $ \cliNode ->
+          withConnection cliNode serverAddr $ \_ ->
+            atomically $ (,) <$> takeTMVar sv <*> takeTMVar cv
 
     return (res == mapAccumL f 0 xs)
 
@@ -208,15 +205,13 @@ prop_socket_client_connect_error _ xs = ioProperty $ do
             protocols   = cliPeers
           }
 
-    nn <- runNetworkNodeWithSocket ni
-    mconn <- try @IO @IOException $ connectTo nn clientAddr
-    r <- case mconn of
-      -- XXX Disregarding the exact exception type
-      Left _     -> return $ property True
-      Right conn -> terminate conn >> return (property False)
-    killNode nn
+    (res :: Either IOException Bool)
+      <- try $ withNetworkNode ni $ \nn ->
+                 withConnection nn clientAddr $ \_ -> pure False
 
-    return r
+    -- XXX Disregarding the exact exception type 
+    pure $ either (const True) id res
+
 
 demo :: forall block .
         (Chain.HasHeader block, Serialise block, Eq block, Show block )
@@ -249,26 +244,21 @@ demo chain0 updates = do
               protocols   = producerPeers
             }
 
-    producerNode <- runNetworkNodeWithSocket producerNet
-    consumerNode <- runNetworkNodeWithSocket consumerNet
+    withNetworkNode producerNet $ \_ ->
+      withNetworkNode consumerNet $  \consumerNode ->
+        withConnection consumerNode (nodeAddress producerNet) $ \_ -> do
+          void $ fork $ sequence_
+              [ do
+                  threadDelay 10e-3 -- just to provide interest
+                  atomically $ do
+                    p <- readTVar producerVar
+                    let Just p' = CPS.applyChainUpdate update p
+                    writeTVar producerVar p'
+              | update <- updates
+              ]
 
-    r <- withConnection consumerNode (nodeAddress producerNet) $ \_ -> do
+          atomically $ takeTMVar done
 
-      void $ fork $ sequence_
-          [ do threadDelay 10e-3 -- just to provide interest
-               atomically $ do
-                  p <- readTVar producerVar
-                  let Just p' = CPS.applyChainUpdate update p
-                  writeTVar producerVar p'
-          | update <- updates
-          ]
-
-      atomically $ takeTMVar done
-
-    killNode producerNode
-    killNode consumerNode
-
-    return r
   where
     checkTip target consumerVar = atomically $ do
       chain <- readTVar consumerVar