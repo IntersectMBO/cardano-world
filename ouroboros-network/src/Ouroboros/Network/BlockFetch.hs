xiaobaicai2018/ouroboros-network

BlockFetch: use headers for the current chain, not blocks

ouroboros-network/src/Ouroboros/Network/BlockFetch.hs
@@ -122,7 +122,7 @@ data BlockFetchConsensusInterface peer header block m =
        -- This must contain info on the last @K@ blocks (unless we're near
        -- the chain genesis of course).
        --
-       readCurrentChain       :: STM m (ChainFragment block),
+       readCurrentChain       :: STM m (ChainFragment header),
 
        -- | Read the current fetch mode that the block fetch logic should use.
        --
@@ -152,7 +152,7 @@ data BlockFetchConsensusInterface peer header block m =
        -- with operational key certificates there are also cases where
        -- we would consider a chain of equal length to the current chain.
        --
-       plausibleCandidateChain :: ChainFragment block
+       plausibleCandidateChain :: ChainFragment header
                                -> ChainFragment header -> Bool,
 
        -- | Compare two candidate chains and return a preference ordering.

IntersectMBO/cardano-world

ouroboros-network/src/Ouroboros/Network/BlockFetch.hs
@@ -130,7 +130,7 @@ data BlockFetchConsensusInterface peer header block m =
        -- This must contain info on the last @K@ blocks (unless we're near
        -- the chain genesis of course).
        --
-       readCurrentChain       :: STM m (AnchoredFragment block),
+       readCurrentChain       :: STM m (AnchoredFragment header),
 
        -- | Read the current fetch mode that the block fetch logic should use.
        --
@@ -160,7 +160,7 @@ data BlockFetchConsensusInterface peer header block m =
        -- with operational key certificates there are also cases where
        -- we would consider a chain of equal length to the current chain.
        --
-       plausibleCandidateChain :: AnchoredFragment block
+       plausibleCandidateChain :: AnchoredFragment header
                                -> AnchoredFragment header -> Bool,
 
        -- | Compare two candidate chains and return a preference ordering.
@@ -209,7 +209,7 @@ blockFetchLogic decisionTracer
       fetchTriggerVariables
       fetchNonTriggerVariables
   where
-    fetchDecisionPolicy :: FetchDecisionPolicy header block
+    fetchDecisionPolicy :: FetchDecisionPolicy header
     fetchDecisionPolicy =
       FetchDecisionPolicy {
         -- TODO: This is a protocol constant, but determined elsewhere.
@@ -225,7 +225,7 @@ blockFetchLogic decisionTracer
         blockFetchSize
       }
 
-    fetchTriggerVariables :: FetchTriggerVariables peer header block m
+    fetchTriggerVariables :: FetchTriggerVariables peer header m
     fetchTriggerVariables =
       FetchTriggerVariables {
         readStateCurrentChain    = readCurrentChain,