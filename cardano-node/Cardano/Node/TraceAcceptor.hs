b7c7a04c15822fa270a280aa7026399b30894cdc
Jordan Millar <jordan.millar@iohk.io>
2019-08-12 11:42:34 -0400

    Move remaining modules from `app` dir to `cardano-node` dir: Part 2

---
+++ cardano-node/Cardano/Node/TraceAcceptor.hs
@@ -0,0 +1,24 @@
+module Cardano.Node.TraceAcceptor (
+      handleTraceAcceptor
+    ) where
+
+import           Cardano.Prelude
+import           Prelude (String)
+
+import           Control.Concurrent (threadDelay)
+import           Control.Monad (forever)
+import           Control.Tracer
+
+{-------------------------------------------------------------------------------
+  Main logic
+-------------------------------------------------------------------------------}
+
+handleTraceAcceptor :: Tracer IO String
+                    -> IO ()
+handleTraceAcceptor tracer = do
+
+    traceWith tracer $ "**************************************"
+    traceWith tracer $ "I am TraceAcceptor"
+    traceWith tracer $ "**************************************"
+
+    forever $ threadDelay 10000000 -- sleep 10s
