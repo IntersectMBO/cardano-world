hamishmack/cardano-node

Update `SubmitTx` to only require a socket path and tx file

cardano-node/src/Cardano/Wallet/Run.hs
@@ -41,7 +41,7 @@ runClient WalletCLI{..} tracer = do
                         (ncUpdate nc)
                         (ncProtocol nc)
     let socketDir = socketFile $ mscFp cliNodeCLI
-    runWalletClient p socketDir cliCoreNodeId tracer'
+    runWalletClient p socketDir tracer'
 
 data WalletCLI = WalletCLI {
     cliCoreNodeId   :: CoreNodeId,

IntersectMBO/cardano-world

cardano-node/src/Cardano/Wallet/Run.hs
@@ -49,7 +49,7 @@ runClient (WalletCLI config delegCertFile gHash gFile sKeyFile socketFp) tracer 
                         Left err -> (putTextLn $ renderError err) >> exitFailure
                         Right (SomeProtocol p) -> pure $ SomeProtocol p
 
-    runWalletClient p socketFp coreNodeId tracer'
+    runWalletClient p socketFp tracer'
 
 renderError :: ProtocolInstantiationError -> Text
 renderError = pack . show