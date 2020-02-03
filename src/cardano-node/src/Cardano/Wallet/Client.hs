hamishmack/cardano-node

Implement `SocketPath`

cardano-node/src/Cardano/Wallet/Client.hs
@@ -49,6 +49,7 @@ import           Ouroboros.Network.Protocol.Handshake.Version
 import           Ouroboros.Network.NodeToClient
 
 import           Cardano.Common.LocalSocket
+import           Cardano.Config.Types (SocketPath)
 import           Cardano.Tracing.Tracers (TraceConstraints)
 
 runWalletClient :: forall blk.
@@ -56,13 +57,13 @@ runWalletClient :: forall blk.
                    , TraceConstraints blk
                    )
                 => Protocol blk
-                -> FilePath
+                -> SocketPath
                 -> CoreNodeId
                 -> Tracer IO String
                 -> IO ()
-runWalletClient ptcl sockDir (CoreNodeId id) tracer = do
+runWalletClient ptcl sockFp (CoreNodeId id) tracer = do
 
-    addr <- localSocketAddrInfo (Just $ CoreId id) sockDir NoMkdirIfMissing
+    addr <- localSocketAddrInfo sockFp
 
     let ProtocolInfo{pInfoConfig} = protocolInfo ptcl
 

IntersectMBO/cardano-world

src/cardano-node/src/Cardano/Wallet/Client.hs
@@ -49,6 +49,7 @@ import           Ouroboros.Network.Protocol.Handshake.Version
 import           Ouroboros.Network.NodeToClient
 
 import           Cardano.Common.LocalSocket
+import           Cardano.Config.Types (SocketPath)
 import           Cardano.Tracing.Tracers (TraceConstraints)
 
 runWalletClient :: forall blk.
@@ -56,13 +57,13 @@ runWalletClient :: forall blk.
                    , TraceConstraints blk
                    )
                 => Protocol blk
-                -> FilePath
+                -> SocketPath
                 -> CoreNodeId
                 -> Tracer IO String
                 -> IO ()
-runWalletClient ptcl sockDir nid tracer = do
+runWalletClient ptcl sockFp (CoreNodeId _) tracer = do
 
-    addr <- localSocketAddrInfo (Just (CoreId nid)) sockDir NoMkdirIfMissing
+    addr <- localSocketAddrInfo sockFp
 
     let ProtocolInfo{pInfoConfig} = protocolInfo ptcl
 