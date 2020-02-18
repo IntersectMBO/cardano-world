skokasik/ouroboros-network

Introduce Acceptable for version neg.
Instead of demanding that all version data is identical (Eq) we relax
the requirement. Usefull for version negotiation with data other
than NetworkMagic.

ouroboros-network/demo/chain-sync.hs
@@ -186,7 +186,7 @@ serverPingPong = do
       networkState
       defaultLocalSocketAddrInfo
       cborTermVersionDataCodec
-      (\(DictVersion _) -> acceptEq)
+      (\(DictVersion _) -> acceptV)
       (simpleSingletonVersions
         (0::Int)
         (NodeToNodeVersionData $ NetworkMagic 0)
@@ -292,7 +292,7 @@ serverPingPong2 = do
       networkState
       defaultLocalSocketAddrInfo
       cborTermVersionDataCodec
-      (\(DictVersion _) -> acceptEq)
+      (\(DictVersion _) -> acceptV)
       (simpleSingletonVersions
         (0::Int)
         (NodeToNodeVersionData $ NetworkMagic 0)
@@ -372,7 +372,7 @@ serverChainSync sockAddr = do
       networkState
       (mkLocalSocketAddrInfo sockAddr)
       cborTermVersionDataCodec
-      (\(DictVersion _) -> acceptEq)
+      (\(DictVersion _) -> acceptV)
       (simpleSingletonVersions
         (0::Int)
         (NodeToNodeVersionData $ NetworkMagic 0)
@@ -578,7 +578,7 @@ serverBlockFetch sockAddr = do
       networkState
       (mkLocalSocketAddrInfo sockAddr)
       cborTermVersionDataCodec
-      (\(DictVersion _) -> acceptEq)
+      (\(DictVersion _) -> acceptV)
       (simpleSingletonVersions
         (0::Int)
         (NodeToNodeVersionData $ NetworkMagic 0)

ouroboros-network/protocol-tests/Ouroboros/Network/Protocol/Handshake/Test.hs
@@ -109,6 +109,10 @@ instance Serialise VersionNumber where
 data Data_0 = C0 | C1 | C2
   deriving (Eq, Show, Typeable, Generic)
 
+instance Acceptable Data_0 where
+  acceptV l r | l == r = Accept
+              | otherwise =  Refuse $ T.pack ""
+
 data0CodecCBORTerm :: CodecCBORTerm Text Data_0
 data0CodecCBORTerm = CodecCBORTerm {encodeTerm, decodeTerm}
     where
@@ -136,6 +140,10 @@ instance CoArbitrary Data_0 where
 data Data_1 = Data_1 Bool
   deriving (Eq, Show, Typeable, Generic)
 
+instance Acceptable Data_1 where
+  acceptV l r | l == r = Accept
+              | otherwise =  Refuse $ T.pack ""
+
 data1CodecCBORTerm :: CodecCBORTerm Text Data_1
 data1CodecCBORTerm = CodecCBORTerm {encodeTerm, decodeTerm}
     where
@@ -155,6 +163,10 @@ instance CoArbitrary Data_1
 --
 data Data_2 = Data_2 Word Word
   deriving (Eq, Show, Typeable, Generic)
+
+instance Acceptable Data_2 where
+  acceptV l r | l == r = Accept
+              | otherwise =  Refuse $ T.pack ""
 
 data2CodecCBORTerm :: CodecCBORTerm Text Data_2
 data2CodecCBORTerm = CodecCBORTerm {encodeTerm, decodeTerm}
@@ -335,7 +347,7 @@ prop_connect :: ArbitraryVersions -> Property
 prop_connect (ArbitraryVersions clientVersions serverVersions) =
   let (serverRes, clientRes) = pureHandshake
         (\(DictVersion _) -> Dict)
-        (\(DictVersion _) -> (==))
+        (\(DictVersion _) vData vData' -> acceptV vData vData' == Accept)
         serverVersions
         clientVersions
   in case runSimOrThrow
@@ -345,7 +357,7 @@ prop_connect (ArbitraryVersions clientVersions serverVersions) =
                 clientVersions)
               (handshakeServerPeer
                 cborTermVersionDataCodec
-                (\(DictVersion _) vData vData' -> bool (Refuse $ T.pack "refused") Accept $ vData == vData')
+                (\(DictVersion _) -> acceptV)
                 serverVersions)) of
       (clientRes', serverRes', TerminalStates TokDone TokDone) ->
            maybe False id clientRes === either (const False) id clientRes'
@@ -368,7 +380,7 @@ prop_channel :: ( MonadAsync m
 prop_channel createChannels clientVersions serverVersions =
   let (serverRes, clientRes) = pureHandshake
         (\(DictVersion _) -> Dict)
-        (\(DictVersion _) -> (==))
+        (\(DictVersion _) vData vData' -> acceptV vData vData' == Accept)
         serverVersions
         clientVersions
   in do
@@ -380,9 +392,9 @@ prop_channel createChannels clientVersions serverVersions =
           clientVersions)
         (handshakeServerPeer
           cborTermVersionDataCodec
-          (\(DictVersion _) vData vData' -> bool (Refuse $ T.pack "") Accept $ vData == vData')
+          (\(DictVersion _) -> acceptV)
           serverVersions)
-    return $ 
+    return $
            maybe False id clientRes === either (const False) id clientRes'
       .&&.
            maybe False id serverRes === either (const False) id serverRes'

ouroboros-network/src/Ouroboros/Network/Protocol/Handshake/Version.hs
@@ -10,7 +10,8 @@ module Ouroboros.Network.Protocol.Handshake.Version
   , Application (..)
   , Version (..)
   , Sigma (..)
-
+  , Accept (..)
+  , Acceptable (..)
   , Dict (..)
   , DictVersion (..)
   , pickVersions
@@ -60,6 +61,18 @@ simpleSingletonVersions vNum vData extra r =
 data Sigma f where
   Sigma :: t -> f t -> Sigma f
 
+
+-- |
+-- A @'Maybe'@ like type which better explains its purpose.
+--
+data Accept
+  = Accept
+  | Refuse !Text
+  deriving (Eq, Show)
+
+class Acceptable v where
+  acceptV :: v -> v -> Accept
+
 -- | Takes a pair of version data: local then remote.
 newtype Application r vData = Application
   { runApplication :: vData -> vData -> r
@@ -79,7 +92,7 @@ data Dict constraint thing where
 
 data DictVersion vData where
      DictVersion :: ( Typeable vData
-                    , Eq vData
+                    , Acceptable vData
                     , Show vData
                     )
                  => CodecCBORTerm Text vData

ouroboros-network/test/Test/Socket.hs
@@ -60,10 +60,10 @@ import qualified Ouroboros.Network.Protocol.ChainSync.Client as ChainSync
 import qualified Ouroboros.Network.Protocol.ChainSync.Codec as ChainSync
 import qualified Ouroboros.Network.Protocol.ChainSync.Examples as ChainSync
 import qualified Ouroboros.Network.Protocol.ChainSync.Server as ChainSync
-import           Ouroboros.Network.Protocol.Handshake.Type (acceptEq,
-                     cborTermVersionDataCodec)
+import           Ouroboros.Network.Protocol.Handshake.Type
+                     (cborTermVersionDataCodec)
 import           Ouroboros.Network.Protocol.Handshake.Version
-                     (simpleSingletonVersions)
+                     (acceptV, simpleSingletonVersions)
 import           Ouroboros.Network.Testing.Serialise
 
 import           Test.ChainGenerators (TestBlockChainAndUpdates (..))
@@ -241,7 +241,7 @@ prop_socket_send_recv initiatorAddr responderAddr f xs = do
         networkState
         responderAddr
         cborTermVersionDataCodec
-        (\(DictVersion _) -> acceptEq)
+        (\(DictVersion _) -> acceptV)
         (simpleSingletonVersions
           NodeToNodeV_1
           (NodeToNodeVersionData $ NetworkMagic 0)
@@ -406,7 +406,7 @@ demo chain0 updates = do
       networkState
       producerAddress
       cborTermVersionDataCodec
-      (\(DictVersion _) -> acceptEq)
+      (\(DictVersion _) -> acceptV)
       (simpleSingletonVersions
         (0::Int)
         (NodeToNodeVersionData $ NetworkMagic 0)

ouroboros-network/test/Test/Subscription.hs
@@ -48,8 +48,8 @@ import qualified Network.TypedProtocol.ReqResp.Server as ReqResp
 import qualified Network.TypedProtocol.ReqResp.Codec.CBOR as ReqResp
 import qualified Network.TypedProtocol.ReqResp.Examples   as ReqResp
 
-import           Ouroboros.Network.Protocol.Handshake.Type (acceptEq, cborTermVersionDataCodec)
-import           Ouroboros.Network.Protocol.Handshake.Version (simpleSingletonVersions)
+import           Ouroboros.Network.Protocol.Handshake.Type (cborTermVersionDataCodec)
+import           Ouroboros.Network.Protocol.Handshake.Version (acceptV, simpleSingletonVersions)
 
 
 import           Ouroboros.Network.Magic
@@ -552,7 +552,7 @@ prop_send_recv f xs first = ioProperty $ do
         (NetworkMutableState tbl peerStatesVar)
         responderAddr
         cborTermVersionDataCodec
-        (\(DictVersion _) -> acceptEq)
+        (\(DictVersion _) -> acceptV)
         (simpleSingletonVersions
           NodeToNodeV_1
           (NodeToNodeVersionData $ NetworkMagic 0)
@@ -684,7 +684,7 @@ prop_send_recv_init_and_rsp f xs = ioProperty $ do
         (NetworkMutableState tbl stVar)
         responderAddr
         cborTermVersionDataCodec
-        (\(DictVersion _) -> acceptEq)
+        (\(DictVersion _) -> acceptV)
         (simpleSingletonVersions
           NodeToNodeV_1
           (NodeToNodeVersionData $ NetworkMagic 0)
@@ -703,7 +703,7 @@ prop_send_recv_init_and_rsp f xs = ioProperty $ do
         (NetworkMutableState tbl stVar)
         responderAddr
         cborTermVersionDataCodec
-        (\(DictVersion _) -> acceptEq)
+        (\(DictVersion _) -> acceptV)
         ((simpleSingletonVersions
           NodeToNodeV_1
           (NodeToNodeVersionData $ NetworkMagic 0)
@@ -816,7 +816,7 @@ _demo = ioProperty $ do
             (NetworkMutableState tbl stVar)
             addr
             cborTermVersionDataCodec
-            (\(DictVersion _) -> acceptEq)
+            (\(DictVersion _) -> acceptV)
             (simpleSingletonVersions
                 NodeToNodeV_1
                 (NodeToNodeVersionData $ NetworkMagic 0)

IntersectMBO/cardano-world

src/ouroboros-network/demo/chain-sync.hs
@@ -196,7 +196,7 @@ serverPingPong = withIOManager $ \iocp -> do
       networkState
       defaultLocalSocketAddr
       cborTermVersionDataCodec
-      (\(DictVersion _) -> acceptEq)
+      (\(DictVersion _) -> acceptableVersion)
       (simpleSingletonVersions
         (0::Int)
         (NodeToNodeVersionData $ NetworkMagic 0)
@@ -304,7 +304,7 @@ serverPingPong2 = withIOManager $ \iocp -> do
       networkState
       defaultLocalSocketAddr
       cborTermVersionDataCodec
-      (\(DictVersion _) -> acceptEq)
+      (\(DictVersion _) -> acceptableVersion)
       (simpleSingletonVersions
         (0::Int)
         (NodeToNodeVersionData $ NetworkMagic 0)
@@ -387,7 +387,7 @@ serverChainSync sockAddr = withIOManager $ \iocp -> do
       networkState
       (localAddressFromPath sockAddr)
       cborTermVersionDataCodec
-      (\(DictVersion _) -> acceptEq)
+      (\(DictVersion _) -> acceptableVersion)
       (simpleSingletonVersions
         (0::Int)
         (NodeToNodeVersionData $ NetworkMagic 0)
@@ -595,7 +595,7 @@ serverBlockFetch sockAddr = withIOManager $ \iocp -> do
       networkState
       (localAddressFromPath sockAddr)
       cborTermVersionDataCodec
-      (\(DictVersion _) -> acceptEq)
+      (\(DictVersion _) -> acceptableVersion)
       (simpleSingletonVersions
         (0::Int)
         (NodeToNodeVersionData $ NetworkMagic 0)

src/ouroboros-network/protocol-tests/Ouroboros/Network/Protocol/Handshake/Test.hs
@@ -109,6 +109,10 @@ instance Serialise VersionNumber where
 data Data_0 = C0 | C1 | C2
   deriving (Eq, Show, Typeable, Generic)
 
+instance Acceptable Data_0 where
+  acceptableVersion l r | l == r = Accept
+                        | otherwise =  Refuse $ T.pack ""
+
 data0CodecCBORTerm :: CodecCBORTerm Text Data_0
 data0CodecCBORTerm = CodecCBORTerm {encodeTerm, decodeTerm}
     where
@@ -136,6 +140,10 @@ instance CoArbitrary Data_0 where
 data Data_1 = Data_1 Bool
   deriving (Eq, Show, Typeable, Generic)
 
+instance Acceptable Data_1 where
+  acceptableVersion l r | l == r = Accept
+                        | otherwise =  Refuse $ T.pack ""
+
 data1CodecCBORTerm :: CodecCBORTerm Text Data_1
 data1CodecCBORTerm = CodecCBORTerm {encodeTerm, decodeTerm}
     where
@@ -155,6 +163,10 @@ instance CoArbitrary Data_1
 --
 data Data_2 = Data_2 Word Word
   deriving (Eq, Show, Typeable, Generic)
+
+instance Acceptable Data_2 where
+  acceptableVersion l r | l == r = Accept
+                        | otherwise =  Refuse $ T.pack ""
 
 data2CodecCBORTerm :: CodecCBORTerm Text Data_2
 data2CodecCBORTerm = CodecCBORTerm {encodeTerm, decodeTerm}
@@ -324,7 +336,7 @@ prop_arbitrary_ArbitraryVersions (ArbitraryVersions (Versions vs) (Versions vs')
 
     property True
   where
-    intersection = vs `Map.intersection` vs' 
+    intersection = vs `Map.intersection` vs'
     intersect    = not (Map.null intersection)
 
     knownVersionNumbers = Map.keys vs'
@@ -335,7 +347,7 @@ prop_connect :: ArbitraryVersions -> Property
 prop_connect (ArbitraryVersions clientVersions serverVersions) =
   let (serverRes, clientRes) = pureHandshake
         (\(DictVersion _) -> Dict)
-        (\(DictVersion _) -> (==))
+        (\(DictVersion _) vData vData' -> acceptableVersion vData vData' == Accept)
         serverVersions
         clientVersions
   in case runSimOrThrow
@@ -345,7 +357,7 @@ prop_connect (ArbitraryVersions clientVersions serverVersions) =
                 clientVersions)
               (handshakeServerPeer
                 cborTermVersionDataCodec
-                (\(DictVersion _) vData vData' -> bool (Refuse $ T.pack "refused") Accept $ vData == vData')
+                (\(DictVersion _) -> acceptableVersion)
                 serverVersions)) of
       (clientRes', serverRes', TerminalStates TokDone TokDone) ->
            maybe False id clientRes === either (const False) id clientRes'
@@ -368,7 +380,7 @@ prop_channel :: ( MonadAsync m
 prop_channel createChannels clientVersions serverVersions =
   let (serverRes, clientRes) = pureHandshake
         (\(DictVersion _) -> Dict)
-        (\(DictVersion _) -> (==))
+        (\(DictVersion _) vData vData' -> acceptableVersion vData vData' == Accept)
         serverVersions
         clientVersions
   in do
@@ -380,9 +392,9 @@ prop_channel createChannels clientVersions serverVersions =
           clientVersions)
         (handshakeServerPeer
           cborTermVersionDataCodec
-          (\(DictVersion _) vData vData' -> bool (Refuse $ T.pack "") Accept $ vData == vData')
+          (\(DictVersion _) -> acceptableVersion)
           serverVersions)
-    return $ 
+    return $
            maybe False id clientRes === either (const False) id clientRes'
       .&&.
            maybe False id serverRes === either (const False) id serverRes'

src/ouroboros-network/src/Ouroboros/Network/Protocol/Handshake/Version.hs
@@ -11,7 +11,8 @@ module Ouroboros.Network.Protocol.Handshake.Version
   , Application (..)
   , Version (..)
   , Sigma (..)
-
+  , Accept (..)
+  , Acceptable (..)
   , Dict (..)
   , DictVersion (..)
   , pickVersions
@@ -74,6 +75,18 @@ simpleSingletonVersions vNum vData extra r =
 data Sigma f where
   Sigma :: !t -> !(f t) -> Sigma f
 
+
+-- |
+-- A @'Maybe'@ like type which better explains its purpose.
+--
+data Accept
+  = Accept
+  | Refuse !Text
+  deriving (Eq, Show)
+
+class Acceptable v where
+  acceptableVersion :: v -> v -> Accept
+
 -- | Takes a pair of version data: local then remote.
 newtype Application r vData = Application
   { runApplication :: vData -> vData -> r
@@ -93,7 +106,7 @@ data Dict constraint thing where
 
 data DictVersion vData where
      DictVersion :: ( Typeable vData
-                    , Eq vData
+                    , Acceptable vData
                     , Show vData
                     )
                  => CodecCBORTerm Text vData

src/ouroboros-network/test/Test/Socket.hs
@@ -70,10 +70,10 @@ import qualified Ouroboros.Network.Protocol.ChainSync.Client as ChainSync
 import qualified Ouroboros.Network.Protocol.ChainSync.Codec as ChainSync
 import qualified Ouroboros.Network.Protocol.ChainSync.Examples as ChainSync
 import qualified Ouroboros.Network.Protocol.ChainSync.Server as ChainSync
-import           Ouroboros.Network.Protocol.Handshake.Type (acceptEq,
-                     cborTermVersionDataCodec)
+import           Ouroboros.Network.Protocol.Handshake.Type
+                     (cborTermVersionDataCodec)
 import           Ouroboros.Network.Protocol.Handshake.Version
-                     (simpleSingletonVersions)
+                     (acceptableVersion, simpleSingletonVersions)
 import           Ouroboros.Network.Testing.Serialise
 
 import           Test.ChainGenerators (TestBlockChainAndUpdates (..))
@@ -254,7 +254,7 @@ prop_socket_send_recv initiatorAddr responderAddr f xs = withIOManager $ \iocp -
         networkState
         responderAddr
         cborTermVersionDataCodec
-        (\(DictVersion _) -> acceptEq)
+        (\(DictVersion _) -> acceptableVersion)
         (simpleSingletonVersions
           NodeToNodeV_1
           (NodeToNodeVersionData $ NetworkMagic 0)
@@ -433,7 +433,7 @@ demo chain0 updates = withIOManager $ \iocp -> do
       networkState
       producerAddress
       cborTermVersionDataCodec
-      (\(DictVersion _) -> acceptEq)
+      (\(DictVersion _) -> acceptableVersion)
       (simpleSingletonVersions
         (0::Int)
         (NodeToNodeVersionData $ NetworkMagic 0)

src/ouroboros-network/test/Test/Subscription.hs
@@ -48,8 +48,8 @@ import qualified Network.TypedProtocol.ReqResp.Server as ReqResp
 import qualified Network.TypedProtocol.ReqResp.Codec.CBOR as ReqResp
 import qualified Network.TypedProtocol.ReqResp.Examples   as ReqResp
 
-import           Ouroboros.Network.Protocol.Handshake.Type (acceptEq, cborTermVersionDataCodec)
-import           Ouroboros.Network.Protocol.Handshake.Version (simpleSingletonVersions)
+import           Ouroboros.Network.Protocol.Handshake.Type (cborTermVersionDataCodec)
+import           Ouroboros.Network.Protocol.Handshake.Version (acceptableVersion, simpleSingletonVersions)
 
 
 import           Ouroboros.Network.IOManager
@@ -559,7 +559,7 @@ prop_send_recv f xs _first = ioProperty $ withIOManager $ \iocp -> do
         (NetworkMutableState tbl peerStatesVar)
         (Socket.addrAddress responderAddr)
         cborTermVersionDataCodec
-        (\(DictVersion _) -> acceptEq)
+        (\(DictVersion _) -> acceptableVersion)
         (simpleSingletonVersions
           NodeToNodeV_1
           (NodeToNodeVersionData $ NetworkMagic 0)
@@ -698,7 +698,7 @@ prop_send_recv_init_and_rsp f xs = ioProperty $ withIOManager $ \iocp -> do
         (NetworkMutableState tbl stVar)
         responderAddr
         cborTermVersionDataCodec
-        (\(DictVersion _) -> acceptEq)
+        (\(DictVersion _) -> acceptableVersion)
         (simpleSingletonVersions
           NodeToNodeV_1
           (NodeToNodeVersionData $ NetworkMagic 0)
@@ -720,7 +720,7 @@ prop_send_recv_init_and_rsp f xs = ioProperty $ withIOManager $ \iocp -> do
           (NetworkMutableState tbl stVar)
           responderAddr
           cborTermVersionDataCodec
-          (\(DictVersion _) -> acceptEq)
+          (\(DictVersion _) -> acceptableVersion)
           (simpleSingletonVersions
             NodeToNodeV_1
             (NodeToNodeVersionData $ NetworkMagic 0)
@@ -839,7 +839,7 @@ _demo = ioProperty $ withIOManager $ \iocp -> do
             (NetworkMutableState tbl stVar)
             (Socket.addrAddress addr)
             cborTermVersionDataCodec
-            (\(DictVersion _) -> acceptEq)
+            (\(DictVersion _) -> acceptableVersion)
             (simpleSingletonVersions
                 NodeToNodeV_1
                 (NodeToNodeVersionData $ NetworkMagic 0)