LintCroft/cardano-node

Accept Bech32 verification keys in Shelley node CLI

cardano-cli/src/Cardano/CLI/Shelley/Parsers.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Cardano.CLI.Shelley.Parsers
   ( -- * CLI command parser
@@ -555,11 +557,11 @@ pNodeCmd =
 
     pKeyHashVRF :: Parser NodeCmd
     pKeyHashVRF =
-      NodeKeyHashVRF <$> pVerificationKeyFile Input <*> pMaybeOutputFile
+      NodeKeyHashVRF <$> pVerificationKeyOrFile AsVrfKey <*> pMaybeOutputFile
 
     pNewCounter :: Parser NodeCmd
     pNewCounter =
-      NodeNewCounter <$> pColdVerificationKeyFile
+      NodeNewCounter <$> pColdVerificationKeyOrFile
                      <*> pCounterValue
                      <*> pOperatorCertIssueCounterFile
 
@@ -573,7 +575,7 @@ pNodeCmd =
 
     pIssueOpCert :: Parser NodeCmd
     pIssueOpCert =
-      NodeIssueOpCert <$> pKESVerificationKeyFile
+      NodeIssueOpCert <$> pKesVerificationKeyOrFile
                       <*> pColdSigningKeyFile
                       <*> pOperatorCertIssueCounterFile
                       <*> pKesPeriod
@@ -1119,6 +1121,12 @@ pOutputFile =
       <> Opt.completer (Opt.bashCompleter "file")
       )
 
+pColdVerificationKeyOrFile :: Parser ColdVerificationKeyOrFile
+pColdVerificationKeyOrFile =
+  ColdStakePoolVerificationKey <$> pStakePoolVerificationKey
+    <|> ColdGenesisDelegateVerificationKey <$> pGenesisDelegateVerificationKey
+    <|> ColdVerificationKeyFile <$> pColdVerificationKeyFile
+
 pColdVerificationKeyFile :: Parser VerificationKeyFile
 pColdVerificationKeyFile =
   VerificationKeyFile <$>
@@ -1134,6 +1142,41 @@ pColdVerificationKeyFile =
         <> Opt.internal
         )
     )
+
+pVerificationKey
+  :: forall keyrole.
+     ( HasTextEnvelope (VerificationKey keyrole)
+     , SerialiseAsBech32 (VerificationKey keyrole)
+     )
+  => AsType keyrole
+  -> Parser (VerificationKey keyrole)
+pVerificationKey asType =
+    Opt.option
+      (Opt.eitherReader deserialiseFromBech32OrHex)
+        (  Opt.long "verification-key"
+        <> Opt.metavar "STRING"
+        <> Opt.help "Verification key (Bech32 or hex-encoded)."
+        )
+  where
+    keyFormats :: NonEmpty KeyFormat
+    keyFormats = NE.fromList [KeyFormatBech32, KeyFormatHex]
+
+    deserialiseFromBech32OrHex
+      :: String
+      -> Either String (VerificationKey keyrole)
+    deserialiseFromBech32OrHex str =
+      first (Text.unpack . renderKeyDecodeError) $
+        deserialiseKey (AsVerificationKey asType) keyFormats (BSC.pack str)
+
+pVerificationKeyOrFile
+  :: ( HasTextEnvelope (VerificationKey keyrole)
+     , SerialiseAsBech32 (VerificationKey keyrole)
+     )
+  => AsType keyrole
+  -> Parser (VerificationKeyOrFile keyrole)
+pVerificationKeyOrFile asType =
+  VerificationKeyValue <$> pVerificationKey asType
+    <|> VerificationKeyFilePath <$> pVerificationKeyFile Input
 
 pVerificationKeyFile :: FileDirection -> Parser VerificationKeyFile
 pVerificationKeyFile fdir =
@@ -1259,8 +1302,42 @@ pGenesisDelegateVerificationKeyOrHashOrFile =
   VerificationKeyOrFile <$> pGenesisDelegateVerificationKeyOrFile
     <|> VerificationKeyHash <$> pGenesisDelegateVerificationKeyHash
 
-pKESVerificationKeyFile :: Parser VerificationKeyFile
-pKESVerificationKeyFile =
+pKesVerificationKeyOrFile :: Parser (VerificationKeyOrFile KesKey)
+pKesVerificationKeyOrFile =
+  VerificationKeyValue <$> pKesVerificationKey
+    <|> VerificationKeyFilePath <$> pKesVerificationKeyFile
+
+pKesVerificationKey :: Parser (VerificationKey KesKey)
+pKesVerificationKey =
+    Opt.option
+      (Opt.eitherReader deserialiseVerKey)
+        (  Opt.long "kes-verification-key"
+        <> Opt.metavar "STRING"
+        <> Opt.help "A Bech32 or hex-encoded hot KES verification key."
+        )
+  where
+    asType :: AsType (VerificationKey KesKey)
+    asType = AsVerificationKey AsKesKey
+
+    deserialiseVerKey :: String -> Either String (VerificationKey KesKey)
+    deserialiseVerKey str =
+      case deserialiseFromBech32 asType (Text.pack str) of
+        Right res -> Right res
+
+        -- The input was valid Bech32, but some other error occurred.
+        Left err@(Bech32UnexpectedPrefix _ _) -> Left (displayError err)
+        Left err@(Bech32DataPartToBytesError _) -> Left (displayError err)
+        Left err@(Bech32DeserialiseFromBytesError _) -> Left (displayError err)
+        Left err@(Bech32WrongPrefix _ _) -> Left (displayError err)
+
+        -- The input was not valid Bech32. Attempt to deserialize it as hex.
+        Left (Bech32DecodingError _) ->
+          case deserialiseFromRawBytesHex asType (BSC.pack str) of
+            Just res' -> Right res'
+            Nothing -> Left "Invalid stake pool verification key."
+
+pKesVerificationKeyFile :: Parser VerificationKeyFile
+pKesVerificationKeyFile =
   VerificationKeyFile <$>
     ( Opt.strOption
         (  Opt.long "kes-verification-key-file"

cardano-cli/src/Cardano/CLI/Shelley/Run/Node.hs
@@ -7,7 +7,8 @@ module Cardano.CLI.Shelley.Run.Node
 import           Cardano.Api.TextView (TextViewDescription (..))
 import           Cardano.Api.Typed
 import           Cardano.CLI.Shelley.Commands
-import           Cardano.CLI.Shelley.Key (KeyDecodeError (..), readSigningKeyFileAnyOf)
+import           Cardano.CLI.Shelley.Key (KeyDecodeError, VerificationKeyOrFile,
+                     readSigningKeyFileAnyOf, readVerificationKeyOrFile)
 import           Cardano.CLI.Types (SigningKeyFile (..), VerificationKeyFile (..))
 import           Cardano.Prelude
 import           Control.Monad.Trans.Except (ExceptT)
@@ -31,8 +32,7 @@ renderShelleyNodeCmdError err =
   case err of
     ShelleyNodeCmdReadFileError fileErr -> Text.pack (displayError fileErr)
 
-    ShelleyNodeCmdReadKeyFileError fileErr ->
-      Text.pack (displayError fileErr)
+    ShelleyNodeCmdReadKeyFileError fileErr -> Text.pack (displayError fileErr)
 
     ShelleyNodeCmdWriteFileError fileErr -> Text.pack (displayError fileErr)
 
@@ -117,12 +117,13 @@ runNodeKeyGenVRF (VerificationKeyFile vkeyPath) (SigningKeyFile skeyPath) = do
     skeyDesc = TextViewDescription "VRF Signing Key"
     vkeyDesc = TextViewDescription "VRF Verification Key"
 
-runNodeKeyHashVRF :: VerificationKeyFile -> Maybe OutputFile
+runNodeKeyHashVRF :: VerificationKeyOrFile VrfKey
+                  -> Maybe OutputFile
                   -> ExceptT ShelleyNodeCmdError IO ()
-runNodeKeyHashVRF (VerificationKeyFile vkeyPath) mOutputFp = do
-  vkey <- firstExceptT ShelleyNodeCmdReadFileError
+runNodeKeyHashVRF verKeyOrFile mOutputFp = do
+  vkey <- firstExceptT ShelleyNodeCmdReadKeyFileError
     . newExceptT
-    $ readFileTextEnvelope (AsVerificationKey AsVrfKey) vkeyPath
+    $ readVerificationKeyOrFile AsVrfKey verKeyOrFile
 
   let hexKeyHash = serialiseToRawBytesHex (verificationKeyHash vkey)
 
@@ -131,20 +132,15 @@ runNodeKeyHashVRF (VerificationKeyFile vkeyPath) mOutputFp = do
     Nothing -> liftIO $ BS.putStrLn hexKeyHash
 
 
-runNodeNewCounter :: VerificationKeyFile
+runNodeNewCounter :: ColdVerificationKeyOrFile
                   -> Word
                   -> OpCertCounterFile
                   -> ExceptT ShelleyNodeCmdError IO ()
-runNodeNewCounter (VerificationKeyFile vkeyPath) counter
+runNodeNewCounter coldVerKeyOrFile counter
                   (OpCertCounterFile ocertCtrPath) = do
 
     vkey <- firstExceptT ShelleyNodeCmdReadFileError . newExceptT $
-              readFileTextEnvelopeAnyOf
-                [ FromSomeType (AsVerificationKey AsStakePoolKey) id
-                , FromSomeType (AsVerificationKey AsGenesisDelegateKey)
-                               castVerificationKey
-                ]
-                vkeyPath
+      readColdVerificationKeyOrFile coldVerKeyOrFile
 
     let ocertIssueCounter =
           OperationalCertificateIssueCounter (fromIntegral counter) vkey
@@ -153,7 +149,7 @@ runNodeNewCounter (VerificationKeyFile vkeyPath) counter
       writeFileTextEnvelope ocertCtrPath Nothing ocertIssueCounter
 
 
-runNodeIssueOpCert :: VerificationKeyFile
+runNodeIssueOpCert :: VerificationKeyOrFile KesKey
                    -- ^ This is the hot KES verification key.
                    -> SigningKeyFile
                    -- ^ This is the cold signing key.
@@ -164,7 +160,7 @@ runNodeIssueOpCert :: VerificationKeyFile
                    -- ^ Start of the validity period for this certificate.
                    -> OutputFile
                    -> ExceptT ShelleyNodeCmdError IO ()
-runNodeIssueOpCert (VerificationKeyFile vkeyKesPath)
+runNodeIssueOpCert kesVerKeyOrFile
                    stakePoolSKeyFile
                    (OpCertCounterFile ocertCtrPath)
                    kesPeriod
@@ -174,9 +170,9 @@ runNodeIssueOpCert (VerificationKeyFile vkeyKesPath)
       . newExceptT
       $ readFileTextEnvelope AsOperationalCertificateIssueCounter ocertCtrPath
 
-    verKeyKes <- firstExceptT ShelleyNodeCmdReadFileError
+    verKeyKes <- firstExceptT ShelleyNodeCmdReadKeyFileError
       . newExceptT
-      $ readFileTextEnvelope (AsVerificationKey AsKesKey) vkeyKesPath
+      $ readVerificationKeyOrFile AsKesKey kesVerKeyOrFile
 
     signKey <- firstExceptT ShelleyNodeCmdReadKeyFileError
       . newExceptT
@@ -229,3 +225,22 @@ runNodeIssueOpCert (VerificationKeyFile vkeyKesPath)
                                (SigningKey GenesisDelegateExtendedKey))]
     bech32PossibleBlockIssuers =
       [FromSomeType (AsSigningKey AsStakePoolKey) Left]
+
+-- | Read a cold verification key or file.
+--
+-- If a filepath is provided, it will be interpreted as a text envelope
+-- formatted file.
+readColdVerificationKeyOrFile
+  :: ColdVerificationKeyOrFile
+  -> IO (Either (FileError TextEnvelopeError) (VerificationKey StakePoolKey))
+readColdVerificationKeyOrFile coldVerKeyOrFile =
+  case coldVerKeyOrFile of
+    ColdStakePoolVerificationKey vk -> pure (Right vk)
+    ColdGenesisDelegateVerificationKey vk ->
+      pure $ Right (castVerificationKey vk)
+    ColdVerificationKeyFile (VerificationKeyFile fp) ->
+      readFileTextEnvelopeAnyOf
+        [ FromSomeType (AsVerificationKey AsStakePoolKey) id
+        , FromSomeType (AsVerificationKey AsGenesisDelegateKey) castVerificationKey
+        ]
+        fp

IntersectMBO/cardano-world

cardano-cli/src/Cardano/CLI/Shelley/Parsers.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Cardano.CLI.Shelley.Parsers
   ( -- * CLI command parser
@@ -565,11 +567,11 @@ pNodeCmd =
 
     pKeyHashVRF :: Parser NodeCmd
     pKeyHashVRF =
-      NodeKeyHashVRF <$> pVerificationKeyFile Input <*> pMaybeOutputFile
+      NodeKeyHashVRF <$> pVerificationKeyOrFile AsVrfKey <*> pMaybeOutputFile
 
     pNewCounter :: Parser NodeCmd
     pNewCounter =
-      NodeNewCounter <$> pColdVerificationKeyFile
+      NodeNewCounter <$> pColdVerificationKeyOrFile
                      <*> pCounterValue
                      <*> pOperatorCertIssueCounterFile
 
@@ -583,7 +585,7 @@ pNodeCmd =
 
     pIssueOpCert :: Parser NodeCmd
     pIssueOpCert =
-      NodeIssueOpCert <$> pKESVerificationKeyFile
+      NodeIssueOpCert <$> pKesVerificationKeyOrFile
                       <*> pColdSigningKeyFile
                       <*> pOperatorCertIssueCounterFile
                       <*> pKesPeriod
@@ -1145,6 +1147,12 @@ pOutputFile =
       <> Opt.completer (Opt.bashCompleter "file")
       )
 
+pColdVerificationKeyOrFile :: Parser ColdVerificationKeyOrFile
+pColdVerificationKeyOrFile =
+  ColdStakePoolVerificationKey <$> pStakePoolVerificationKey
+    <|> ColdGenesisDelegateVerificationKey <$> pGenesisDelegateVerificationKey
+    <|> ColdVerificationKeyFile <$> pColdVerificationKeyFile
+
 pColdVerificationKeyFile :: Parser VerificationKeyFile
 pColdVerificationKeyFile =
   VerificationKeyFile <$>
@@ -1160,6 +1168,36 @@ pColdVerificationKeyFile =
         <> Opt.internal
         )
     )
+
+pVerificationKey
+  :: forall keyrole. SerialiseAsBech32 (VerificationKey keyrole)
+  => AsType keyrole
+  -> Parser (VerificationKey keyrole)
+pVerificationKey asType =
+    Opt.option
+      (Opt.eitherReader deserialiseFromBech32OrHex)
+        (  Opt.long "verification-key"
+        <> Opt.metavar "STRING"
+        <> Opt.help "Verification key (Bech32 or hex-encoded)."
+        )
+  where
+    keyFormats :: NonEmpty (InputFormat (VerificationKey keyrole))
+    keyFormats = NE.fromList [InputFormatBech32, InputFormatHex]
+
+    deserialiseFromBech32OrHex
+      :: String
+      -> Either String (VerificationKey keyrole)
+    deserialiseFromBech32OrHex str =
+      first (Text.unpack . renderInputDecodeError) $
+        deserialiseInput (AsVerificationKey asType) keyFormats (BSC.pack str)
+
+pVerificationKeyOrFile
+  :: SerialiseAsBech32 (VerificationKey keyrole)
+  => AsType keyrole
+  -> Parser (VerificationKeyOrFile keyrole)
+pVerificationKeyOrFile asType =
+  VerificationKeyValue <$> pVerificationKey asType
+    <|> VerificationKeyFilePath <$> pVerificationKeyFile Input
 
 pVerificationKeyFile :: FileDirection -> Parser VerificationKeyFile
 pVerificationKeyFile fdir =
@@ -1285,8 +1323,42 @@ pGenesisDelegateVerificationKeyOrHashOrFile =
   VerificationKeyOrFile <$> pGenesisDelegateVerificationKeyOrFile
     <|> VerificationKeyHash <$> pGenesisDelegateVerificationKeyHash
 
-pKESVerificationKeyFile :: Parser VerificationKeyFile
-pKESVerificationKeyFile =
+pKesVerificationKeyOrFile :: Parser (VerificationKeyOrFile KesKey)
+pKesVerificationKeyOrFile =
+  VerificationKeyValue <$> pKesVerificationKey
+    <|> VerificationKeyFilePath <$> pKesVerificationKeyFile
+
+pKesVerificationKey :: Parser (VerificationKey KesKey)
+pKesVerificationKey =
+    Opt.option
+      (Opt.eitherReader deserialiseVerKey)
+        (  Opt.long "kes-verification-key"
+        <> Opt.metavar "STRING"
+        <> Opt.help "A Bech32 or hex-encoded hot KES verification key."
+        )
+  where
+    asType :: AsType (VerificationKey KesKey)
+    asType = AsVerificationKey AsKesKey
+
+    deserialiseVerKey :: String -> Either String (VerificationKey KesKey)
+    deserialiseVerKey str =
+      case deserialiseFromBech32 asType (Text.pack str) of
+        Right res -> Right res
+
+        -- The input was valid Bech32, but some other error occurred.
+        Left err@(Bech32UnexpectedPrefix _ _) -> Left (displayError err)
+        Left err@(Bech32DataPartToBytesError _) -> Left (displayError err)
+        Left err@(Bech32DeserialiseFromBytesError _) -> Left (displayError err)
+        Left err@(Bech32WrongPrefix _ _) -> Left (displayError err)
+
+        -- The input was not valid Bech32. Attempt to deserialize it as hex.
+        Left (Bech32DecodingError _) ->
+          case deserialiseFromRawBytesHex asType (BSC.pack str) of
+            Just res' -> Right res'
+            Nothing -> Left "Invalid stake pool verification key."
+
+pKesVerificationKeyFile :: Parser VerificationKeyFile
+pKesVerificationKeyFile =
   VerificationKeyFile <$>
     ( Opt.strOption
         (  Opt.long "kes-verification-key-file"

cardano-cli/src/Cardano/CLI/Shelley/Run/Node.hs
@@ -7,7 +7,8 @@ module Cardano.CLI.Shelley.Run.Node
 import           Cardano.Api.TextView (TextViewDescription (..))
 import           Cardano.Api.Typed
 import           Cardano.CLI.Shelley.Commands
-import           Cardano.CLI.Shelley.Key (InputDecodeError (..), readSigningKeyFileAnyOf)
+import           Cardano.CLI.Shelley.Key (InputDecodeError, VerificationKeyOrFile,
+                     readSigningKeyFileAnyOf, readVerificationKeyOrFile)
 import           Cardano.CLI.Types (SigningKeyFile (..), VerificationKeyFile (..))
 import           Cardano.Prelude
 import           Control.Monad.Trans.Except (ExceptT)
@@ -31,8 +32,7 @@ renderShelleyNodeCmdError err =
   case err of
     ShelleyNodeCmdReadFileError fileErr -> Text.pack (displayError fileErr)
 
-    ShelleyNodeCmdReadKeyFileError fileErr ->
-      Text.pack (displayError fileErr)
+    ShelleyNodeCmdReadKeyFileError fileErr -> Text.pack (displayError fileErr)
 
     ShelleyNodeCmdWriteFileError fileErr -> Text.pack (displayError fileErr)
 
@@ -117,12 +117,13 @@ runNodeKeyGenVRF (VerificationKeyFile vkeyPath) (SigningKeyFile skeyPath) = do
     skeyDesc = TextViewDescription "VRF Signing Key"
     vkeyDesc = TextViewDescription "VRF Verification Key"
 
-runNodeKeyHashVRF :: VerificationKeyFile -> Maybe OutputFile
+runNodeKeyHashVRF :: VerificationKeyOrFile VrfKey
+                  -> Maybe OutputFile
                   -> ExceptT ShelleyNodeCmdError IO ()
-runNodeKeyHashVRF (VerificationKeyFile vkeyPath) mOutputFp = do
-  vkey <- firstExceptT ShelleyNodeCmdReadFileError
+runNodeKeyHashVRF verKeyOrFile mOutputFp = do
+  vkey <- firstExceptT ShelleyNodeCmdReadKeyFileError
     . newExceptT
-    $ readFileTextEnvelope (AsVerificationKey AsVrfKey) vkeyPath
+    $ readVerificationKeyOrFile AsVrfKey verKeyOrFile
 
   let hexKeyHash = serialiseToRawBytesHex (verificationKeyHash vkey)
 
@@ -131,20 +132,15 @@ runNodeKeyHashVRF (VerificationKeyFile vkeyPath) mOutputFp = do
     Nothing -> liftIO $ BS.putStrLn hexKeyHash
 
 
-runNodeNewCounter :: VerificationKeyFile
+runNodeNewCounter :: ColdVerificationKeyOrFile
                   -> Word
                   -> OpCertCounterFile
                   -> ExceptT ShelleyNodeCmdError IO ()
-runNodeNewCounter (VerificationKeyFile vkeyPath) counter
+runNodeNewCounter coldVerKeyOrFile counter
                   (OpCertCounterFile ocertCtrPath) = do
 
     vkey <- firstExceptT ShelleyNodeCmdReadFileError . newExceptT $
-              readFileTextEnvelopeAnyOf
-                [ FromSomeType (AsVerificationKey AsStakePoolKey) id
-                , FromSomeType (AsVerificationKey AsGenesisDelegateKey)
-                               castVerificationKey
-                ]
-                vkeyPath
+      readColdVerificationKeyOrFile coldVerKeyOrFile
 
     let ocertIssueCounter =
           OperationalCertificateIssueCounter (fromIntegral counter) vkey
@@ -153,7 +149,7 @@ runNodeNewCounter (VerificationKeyFile vkeyPath) counter
       writeFileTextEnvelope ocertCtrPath Nothing ocertIssueCounter
 
 
-runNodeIssueOpCert :: VerificationKeyFile
+runNodeIssueOpCert :: VerificationKeyOrFile KesKey
                    -- ^ This is the hot KES verification key.
                    -> SigningKeyFile
                    -- ^ This is the cold signing key.
@@ -164,7 +160,7 @@ runNodeIssueOpCert :: VerificationKeyFile
                    -- ^ Start of the validity period for this certificate.
                    -> OutputFile
                    -> ExceptT ShelleyNodeCmdError IO ()
-runNodeIssueOpCert (VerificationKeyFile vkeyKesPath)
+runNodeIssueOpCert kesVerKeyOrFile
                    stakePoolSKeyFile
                    (OpCertCounterFile ocertCtrPath)
                    kesPeriod
@@ -174,9 +170,9 @@ runNodeIssueOpCert (VerificationKeyFile vkeyKesPath)
       . newExceptT
       $ readFileTextEnvelope AsOperationalCertificateIssueCounter ocertCtrPath
 
-    verKeyKes <- firstExceptT ShelleyNodeCmdReadFileError
+    verKeyKes <- firstExceptT ShelleyNodeCmdReadKeyFileError
       . newExceptT
-      $ readFileTextEnvelope (AsVerificationKey AsKesKey) vkeyKesPath
+      $ readVerificationKeyOrFile AsKesKey kesVerKeyOrFile
 
     signKey <- firstExceptT ShelleyNodeCmdReadKeyFileError
       . newExceptT
@@ -229,3 +225,22 @@ runNodeIssueOpCert (VerificationKeyFile vkeyKesPath)
                                (SigningKey GenesisDelegateExtendedKey))]
     bech32PossibleBlockIssuers =
       [FromSomeType (AsSigningKey AsStakePoolKey) Left]
+
+-- | Read a cold verification key or file.
+--
+-- If a filepath is provided, it will be interpreted as a text envelope
+-- formatted file.
+readColdVerificationKeyOrFile
+  :: ColdVerificationKeyOrFile
+  -> IO (Either (FileError TextEnvelopeError) (VerificationKey StakePoolKey))
+readColdVerificationKeyOrFile coldVerKeyOrFile =
+  case coldVerKeyOrFile of
+    ColdStakePoolVerificationKey vk -> pure (Right vk)
+    ColdGenesisDelegateVerificationKey vk ->
+      pure $ Right (castVerificationKey vk)
+    ColdVerificationKeyFile (VerificationKeyFile fp) ->
+      readFileTextEnvelopeAnyOf
+        [ FromSomeType (AsVerificationKey AsStakePoolKey) id
+        , FromSomeType (AsVerificationKey AsGenesisDelegateKey) castVerificationKey
+        ]
+        fp