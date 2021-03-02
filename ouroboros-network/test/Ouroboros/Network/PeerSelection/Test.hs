bariscicek/ouroboros-network

p2p-governor: introduce PeerStateActions
It's good to keep peer state changes callbacks in a seprate recrod, they
will likely not change much as we go towards decentralisation.

ouroboros-network/test/Ouroboros/Network/PeerSelection/Test.hs
@@ -237,11 +237,13 @@ mockPeerSelectionActions' tracer
       requestPublicRootPeers   = \_ -> return (publicRootPeers, 60),
       readPeerSelectionTargets = readTVar targetsVar,
       requestPeerGossip,
-      establishPeerConnection,
-      monitorPeerConnection,
-      activatePeerConnection,
-      deactivatePeerConnection,
-      closePeerConnection
+      peerStateActions         = PeerStateActions {
+          establishPeerConnection,
+          monitorPeerConnection,
+          activatePeerConnection,
+          deactivatePeerConnection,
+          closePeerConnection
+        }
     }
   where
     requestPeerGossip addr = do
@@ -1056,11 +1058,13 @@ _governorFindingPublicRoots targetNumberOfRootPeers domains =
                 readPeerSelectionTargets = return targets,
                 requestPeerGossip        = \_ -> return [],
                 requestPublicRootPeers   = \_ -> return (Set.empty, 0),
-                establishPeerConnection  = fail "establishPeerConnection",
-                monitorPeerConnection    = fail "monitorPeerConnection",
-                activatePeerConnection   = fail "activatePeerConnection",
-                deactivatePeerConnection = fail "deactivatePeerConnection",
-                closePeerConnection      = fail "closePeerConnection"
+                peerStateActions         = PeerStateActions {
+                  establishPeerConnection  = fail "establishPeerConnection",
+                  monitorPeerConnection    = fail "monitorPeerConnection",
+                  activatePeerConnection   = fail "activatePeerConnection",
+                  deactivatePeerConnection = fail "deactivatePeerConnection",
+                  closePeerConnection      = fail "closePeerConnection"
+                }
               }
 
     targets :: PeerSelectionTargets

IntersectMBO/cardano-world

ouroboros-network/test/Ouroboros/Network/PeerSelection/Test.hs
@@ -238,11 +238,13 @@ mockPeerSelectionActions' tracer
       requestPublicRootPeers   = \_ -> return (publicRootPeers, 60),
       readPeerSelectionTargets = readTVar targetsVar,
       requestPeerGossip,
-      establishPeerConnection,
-      monitorPeerConnection,
-      activatePeerConnection,
-      deactivatePeerConnection,
-      closePeerConnection
+      peerStateActions         = PeerStateActions {
+          establishPeerConnection,
+          monitorPeerConnection,
+          activatePeerConnection,
+          deactivatePeerConnection,
+          closePeerConnection
+        }
     }
   where
     requestPeerGossip addr = do
@@ -1057,11 +1059,13 @@ _governorFindingPublicRoots targetNumberOfRootPeers domains =
                 readPeerSelectionTargets = return targets,
                 requestPeerGossip        = \_ -> return [],
                 requestPublicRootPeers   = \_ -> return (Set.empty, 0),
-                establishPeerConnection  = error "establishPeerConnection",
-                monitorPeerConnection    = error "monitorPeerConnection",
-                activatePeerConnection   = error "activatePeerConnection",
-                deactivatePeerConnection = error "deactivatePeerConnection",
-                closePeerConnection      = error "closePeerConnection"
+                peerStateActions         = PeerStateActions {
+                  establishPeerConnection  = error "establishPeerConnection",
+                  monitorPeerConnection    = error "monitorPeerConnection",
+                  activatePeerConnection   = error "activatePeerConnection",
+                  deactivatePeerConnection = error "deactivatePeerConnection",
+                  closePeerConnection      = error "closePeerConnection"
+                }
               }
 
     targets :: PeerSelectionTargets