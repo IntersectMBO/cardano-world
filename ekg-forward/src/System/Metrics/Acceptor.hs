e5ed2dede93ac315c8a7869aced0cd221e470292
Denis Shevchenko <denis.shevchenko@iohk.io>
2021-03-05 15:00:43 +0400

    Acceptor top-level, init.

--- ekg-forward/src/System/Metrics/Acceptor.hs
+++ ekg-forward/src/System/Metrics/Acceptor.hs
@@ -5,16 +5,128 @@ Maintainer: Denis Shevchenko <denis.shevchenko@iohk.io>
 See README for more info
 -}
 
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE EmptyCase #-}
 {-# LANGUAGE PolyKinds #-}
 
+-- | This top-level module will be used by the acceptor app
+-- (the app that asks EKG metrics from the forwarder app).
+--
+module System.Metrics.Acceptor (
+  ekgAcceptor
+  ) where
 
-module System.Metrics.Acceptor where
+import qualified Data.ByteString.Lazy as LBS
+import           GHC.Generics (Generic)
+import           Data.Void (Void)
 
--- This top-level module will be used by the acceptor app
--- (the app that asks EKG metrics from the forwarder). 
+import           Control.Concurrent.Async
+import           Control.Tracer
+
+import qualified Codec.Serialise as CBOR
+
+import           Ouroboros.Network.Mux
+import           Ouroboros.Network.NodeToNode
+import           Ouroboros.Network.IOManager
+import           Ouroboros.Network.Snocket
+import           Ouroboros.Network.Socket
+import           Ouroboros.Network.Util.ShowProxy (ShowProxy(..))
+
+import           Ouroboros.Network.Codec
+import           Ouroboros.Network.Protocol.Handshake.Codec
+import           Ouroboros.Network.Protocol.Handshake.Unversioned
+import           Ouroboros.Network.Protocol.Handshake.Version
+
+import qualified System.Metrics.Internal.Protocol.Acceptor as Acceptor
+import qualified System.Metrics.Internal.Protocol.Codec as Acceptor
+import qualified System.Metrics.Internal.Protocol.Type as Acceptor
+
+-- | Please note that acceptor is a server from the __networking__ point of view:
+-- the forwarder establishes network connection with the acceptor.
+--
+ekgAcceptor :: FilePath -> IO Void
+ekgAcceptor sockAddr = withIOManager $ \iocp -> do
+    networkState <- newNetworkMutableState
+    _ <- async $ cleanNetworkMutableState networkState
+    withServerNode
+      (localSnocket iocp defaultLocalSocketAddrPath)
+      nullNetworkServerTracers
+      networkState
+      (AcceptedConnectionsLimit maxBound maxBound 0)
+      (localAddressFromPath sockAddr)
+      unversionedHandshakeCodec
+      (cborTermVersionDataCodec unversionedProtocolDataCodec)
+      acceptableVersion
+      (simpleSingletonVersions
+        UnversionedProtocol
+        UnversionedProtocolData
+        (SomeResponderApplication app))
+      nullErrorPolicies
+      $ \_ serverAsync -> wait serverAsync -- Block until async exception.
+  where
+    app :: OuroborosApplication 'ResponderMode addr LBS.ByteString IO Void ()
+    app =
+      OuroborosApplication $ \_connectionId _shouldStopSTM -> [
+        MiniProtocol {
+          miniProtocolNum    = MiniProtocolNum 2,
+          miniProtocolLimits = maximumMiniProtocolLimits,
+          miniProtocolRun    = acceptEKGMetrics
+        }
+      ]
+
+    acceptEKGMetrics :: RunMiniProtocol 'ResponderMode LBS.ByteString IO Void ()
+    acceptEKGMetrics =
+      ResponderProtocolOnly $
+        MuxPeer
+          (contramap show stdoutTracer)
+          codecEKGForward
+          (Acceptor.ekgAcceptorPeer (ekgAcceptorActions 0))
+
+codecEKGForward
+  :: ( CBOR.Serialise req
+     , CBOR.Serialise resp
+     )
+  => Codec (Acceptor.EKGForward req resp)
+           CBOR.DeserialiseFailure
+           IO LBS.ByteString
+codecEKGForward =
+  Acceptor.codecEKGForward
+    CBOR.encode CBOR.decode
+    CBOR.encode CBOR.decode
+
+data Req = SimpleReq
+  deriving (Generic, Show)
+
+data Resp = SimpleResp ![Int]
+  deriving (Generic, Show)
+
+instance ShowProxy Req where
+  showProxy _ = "SimpleReq"
+
+instance ShowProxy Resp where
+  showProxy _ = "SimpleResp" 
+
+instance CBOR.Serialise Req
+instance CBOR.Serialise Resp
+
+ekgAcceptorActions :: Int -> Acceptor.EKGAcceptor Req Resp IO ()
+ekgAcceptorActions 0 = Acceptor.SendMsgReq SimpleReq (\_resp -> return (ekgAcceptorActions 1))
+ekgAcceptorActions _ = Acceptor.SendMsgDone (return ())
+
+defaultLocalSocketAddrPath :: FilePath
+#if defined(mingw32_HOST_OS)
+defaultLocalSocketAddrPath =  "\\\\.\\pipe\\demo-ekg-forward"
+#else
+defaultLocalSocketAddrPath =  "./demo-ekg-forward.sock"
+#endif
+
+maximumMiniProtocolLimits :: MiniProtocolLimits
+maximumMiniProtocolLimits =
+    MiniProtocolLimits {
+      maximumIngressQueue = maxBound
+    }
--- ekg-forward/src/System/Metrics/Internal/Protocol/Codec.hs
+++ ekg-forward/src/System/Metrics/Internal/Protocol/Codec.hs
@@ -11,74 +11,89 @@ See README for more info
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TypeFamilies #-}
 
 module System.Metrics.Internal.Protocol.Codec where
 
+import           Control.Monad.Class.MonadST
+import qualified Data.ByteString.Lazy as LBS
+import           Data.Word (Word8)
+import           Data.Char (ord)
+import           GHC.Base (unsafeChr)
+import           Text.Printf (printf)
 import           Text.Read (readMaybe)
+
+import           Codec.CBOR.Decoding (decodeListLen, decodeWord)
+import qualified Codec.CBOR.Decoding as CBOR
+import           Codec.CBOR.Encoding (encodeListLen, encodeWord)
+import qualified Codec.CBOR.Encoding as CBOR
+import qualified Codec.CBOR.Read as CBOR
 
 import           Network.TypedProtocol.Core
 import           Ouroboros.Network.Codec
 
 import           System.Metrics.Internal.Protocol.Type
 
-codecEKGForward ::
-    forall req resp m
-  . (Monad m, Show req, Show resp, Read req, Read resp)
-  => Codec (EKGForward req resp) CodecFailure m String
-codecEKGForward =
-  Codec{encode, decode}
+codecEKGForward
+  :: forall req resp m.
+     (MonadST m)
+  => (req -> CBOR.Encoding)
+  -> (forall s . CBOR.Decoder s req)
+  -> (resp -> CBOR.Encoding)
+  -> (forall s . CBOR.Decoder s resp)
+  -> Codec (EKGForward req resp)
+           CBOR.DeserialiseFailure m LBS.ByteString
+codecEKGForward encodeReq  decodeReq
+                encodeResp decodeResp =
+  mkCodecCborLazyBS encode decode
  where
-  encode :: forall req' resp'
-                   (pr  :: PeerRole)
-                   (st  :: EKGForward req' resp')
-                   (st' :: EKGForward req' resp')
-         . (Show (Message (EKGForward req' resp') st st'))
-         => PeerHasAgency pr st
-         -> Message (EKGForward req' resp') st st'
-         -> String
-  encode (ClientAgency TokIdle) msg = show msg ++ "\n"
-  encode (ServerAgency TokBusy) msg = show msg ++ "\n"
+  -- Encode messages.
+  encode :: forall (pr  :: PeerRole)
+                   (st  :: EKGForward req resp)
+                   (st' :: EKGForward req resp).
+            PeerHasAgency pr st
+         -> Message (EKGForward req resp) st st'
+         -> CBOR.Encoding
 
-  decode :: forall req' resp' m'
-                   (pr :: PeerRole)
-                   (st :: EKGForward req' resp')
-         .  (Monad m', Read req', Read resp')
-         => PeerHasAgency pr st
-         -> m' (DecodeStep String CodecFailure m' (SomeMessage st))
-  decode stok =
-    decodeTerminatedFrame '\n' $ \str trailing ->
-      case (stok, break (==' ') str) of
-        -- Please note that, from __interaction__ point of view:
-        -- 1. ClientAgency corresponds to acceptor's agency.
-        -- 3. ServerAgency corresponds to forwarder's agency.
-        (ClientAgency TokIdle, ("MsgReq", str'))
-           | Just resp <- readMaybe str'          -> DecodeDone (SomeMessage (MsgReq resp)) trailing
-        (ServerAgency TokBusy, ("MsgResp", str'))
-           | Just resp <- readMaybe str'          -> DecodeDone (SomeMessage (MsgResp resp)) trailing
-        (ClientAgency TokIdle, ("MsgDone", ""))   -> DecodeDone (SomeMessage MsgDone) trailing
-        -- Unexpected messages!
-        (ServerAgency _      , _     ) -> DecodeFail failure
-          where failure = CodecFailure ("Unexpected message from forwarder: " ++ str)
-        (ClientAgency _      , _     ) -> DecodeFail failure
-          where failure = CodecFailure ("Unexpected message from acceptor: " ++ str)
+  encode (ClientAgency TokIdle) (MsgReq req) =
+    encodeListLen 2
+      <> encodeWord 0
+      <> encodeReq req
 
-decodeTerminatedFrame
-  :: forall m a. Monad m
-  => Char
-  -> (String -> Maybe String -> DecodeStep String CodecFailure m a)
-  -> m (DecodeStep String CodecFailure m a)
-decodeTerminatedFrame terminator k = go []
-  where
-    go :: [String] -> m (DecodeStep String CodecFailure m a)
-    go chunks =
-      return $ DecodePartial $ \mchunk ->
-        case mchunk of
-          Nothing    -> return $ DecodeFail CodecFailureOutOfInput
-          Just chunk ->
-            case break (==terminator) chunk of
-              (c, _:c') -> return $ k (concat (reverse (c:chunks)))
-                                      (if null c' then Nothing else Just c)
-              _         -> go (chunk : chunks)
+  encode (ClientAgency TokIdle) MsgDone =
+    encodeListLen 1
+      <> encodeWord 1
+
+  encode (ServerAgency TokBusy) (MsgResp resp) =
+    encodeListLen 2
+      <> encodeWord 1
+      <> encodeResp resp
+
+  -- Decode messages
+  decode :: forall (pr :: PeerRole)
+                   (st :: EKGForward req resp) s.
+            PeerHasAgency pr st
+         -> CBOR.Decoder s (SomeMessage st)
+  decode stok = do
+    len <- decodeListLen
+    key <- decodeWord
+    case (key, len, stok) of
+      (0, 2, ClientAgency TokIdle) -> do
+        req <- decodeReq
+        return (SomeMessage (MsgReq req))
+
+      (1, 1, ClientAgency TokIdle) ->
+        return (SomeMessage MsgDone)
+
+      (1, 2, ServerAgency TokBusy) -> do
+        resp <- decodeResp
+        return (SomeMessage (MsgResp resp))
+
+      -- Failures per protocol state
+      (_, _, ClientAgency TokIdle) ->
+        fail (printf "codecEKGForward (%s) unexpected key (%d, %d)" (show stok) key len)
+      (_, _, ServerAgency TokBusy) ->
+        fail (printf "codecEKGForward (%s) unexpected key (%d, %d)" (show stok) key len)
