cryptobuks1/ouroboros-network

p2p-governor: introduce EstablishedPeers
EstablisehdPeers keeps track of established peer connection and their
status.  It is using the same logic as KnownPeers to track peers which
are not ready to promote to hot state. This patch  just replaces the
original logic.  This is just a preparation for the following patch.

ouroboros-network/src/Ouroboros/Network/PeerSelection/Governor/ActivePeers.hs
@@ -17,6 +17,7 @@ import           Control.Concurrent.JobPool (Job(..))
 import           Control.Exception (SomeException, assert)
 
 import           Ouroboros.Network.PeerSelection.Types
+import qualified Ouroboros.Network.PeerSelection.EstablishedPeers as EstablishedPeers
 import           Ouroboros.Network.PeerSelection.KnownPeers (KnownPeerInfo(..))
 import qualified Ouroboros.Network.PeerSelection.KnownPeers as KnownPeers
 import           Ouroboros.Network.PeerSelection.Governor.Types
@@ -59,7 +60,7 @@ belowTarget actions
           -- The numPeersToPromote is positive due to the first guard.
       let availableToPromote :: Map peeraddr KnownPeerInfo
           availableToPromote = KnownPeers.toMap knownPeers
-                                `Map.intersection` establishedPeers
+                                `Map.intersection` EstablishedPeers.establishedReady establishedPeers
                                 `Map.withoutKeys` activePeers
                                 `Map.withoutKeys` inProgressPromoteWarm
                                 `Map.withoutKeys` inProgressDemoteWarm
@@ -71,7 +72,7 @@ belowTarget actions
                              availableToPromote
                              numPeersToPromote
       let selectedToPromote' :: Map peeraddr peerconn
-          selectedToPromote' = establishedPeers
+          selectedToPromote' = EstablishedPeers.establishedReady establishedPeers
                                  `Map.restrictKeys` selectedToPromote
       return $ \_now -> Decision {
         decisionTrace = TracePromoteWarmPeers
@@ -90,7 +91,7 @@ belowTarget actions
   = GuardedSkip Nothing
   where
     numEstablishedPeers, numActivePeers, numPromoteInProgress :: Int
-    numEstablishedPeers  = Map.size establishedPeers
+    numEstablishedPeers  = EstablishedPeers.size establishedPeers
     numActivePeers       = Set.size activePeers
     numPromoteInProgress = Set.size inProgressPromoteWarm
     numDemoteInProgress  = Set.size inProgressDemoteWarm
@@ -125,14 +126,15 @@ jobPromoteWarmPeer PeerSelectionActions{peerStateActions = PeerStateActions {act
       -- the responsibility of activatePeerConnection
       activatePeerConnection peerconn
       return $ Completion $ \st _now ->
-        assert (peeraddr `Map.member` establishedPeers st)
+        assert (peeraddr `EstablishedPeers.member` establishedPeers st)
         Decision {
           decisionTrace = TracePromoteWarmDone peeraddr,
           decisionState = st {
                             activePeers           = Set.insert peeraddr
                                                       (activePeers st),
-                            establishedStatus     = Map.insert peeraddr PeerHot
-                                                      (establishedStatus st),
+                            establishedPeers      = EstablishedPeers.updateStatus
+                                                      peeraddr PeerHot
+                                                      (establishedPeers st),
                             inProgressPromoteWarm = Set.delete peeraddr
                                                       (inProgressPromoteWarm st)
                           },
@@ -186,7 +188,7 @@ aboveTarget actions
                             availableToDemote
                             numPeersToDemote
       let selectedToDemote' :: Map peeraddr peerconn
-          selectedToDemote' = establishedPeers
+          selectedToDemote' = EstablishedPeers.allPeers establishedPeers
                                 `Map.restrictKeys` selectedToDemote
 
       return $ \_now -> Decision {
@@ -235,14 +237,14 @@ jobDemoteActivePeer PeerSelectionActions{peerStateActions = PeerStateActions {de
     job = do
       deactivatePeerConnection peerconn
       return $ Completion $ \st _now ->
-        assert (peeraddr `Map.member` establishedPeers st)
+        assert (peeraddr `EstablishedPeers.member` establishedPeers st)
         Decision {
           decisionTrace = TraceDemoteHotDone peeraddr,
           decisionState = st {
                             activePeers         = Set.delete peeraddr
                                                     (activePeers st),
-                            establishedStatus   = Map.insert peeraddr PeerWarm
-                                                    (establishedStatus st),
+                            establishedPeers    = EstablishedPeers.updateStatus peeraddr PeerWarm
+                                                    (establishedPeers st),
                             inProgressDemoteHot = Set.delete peeraddr
                                                     (inProgressDemoteHot st)
                           },

ouroboros-network/src/Ouroboros/Network/PeerSelection/Governor/Types.hs
@@ -19,6 +19,8 @@ import           Control.Monad.Class.MonadSTM
 import           Control.Monad.Class.MonadTime
 import           Control.Exception (assert, SomeException)
 
+import qualified Ouroboros.Network.PeerSelection.EstablishedPeers as EstablishedPeers
+import           Ouroboros.Network.PeerSelection.EstablishedPeers (EstablishedPeers)
 import qualified Ouroboros.Network.PeerSelection.KnownPeers as KnownPeers
 import           Ouroboros.Network.PeerSelection.KnownPeers (KnownPeers, KnownPeerInfo(..))
 import           Ouroboros.Network.PeerSelection.Types
@@ -217,8 +219,7 @@ data PeerSelectionState peeraddr peerconn = PeerSelectionState {
 
        -- |
        --
-       establishedPeers     :: !(Map peeraddr peerconn),
-       establishedStatus    :: !(Map peeraddr PeerStatus),
+       establishedPeers     :: !(EstablishedPeers peeraddr peerconn),
 
        -- |
        --
@@ -261,8 +262,7 @@ emptyPeerSelectionState =
       localRootPeers       = Map.empty,
       publicRootPeers      = Set.empty,
       knownPeers           = KnownPeers.empty,
-      establishedPeers     = Map.empty,
-      establishedStatus    = Map.empty,
+      establishedPeers     = EstablishedPeers.empty,
       activePeers          = Set.empty,
       publicRootBackoffs   = 0,
       publicRootRetryTime  = Time 0,
@@ -283,9 +283,9 @@ assertPeerSelectionState PeerSelectionState{..} =
 
     -- The activePeers is a subset of the establishedPeers
     -- which is a subset of the known peers
- . assert (Set.isSubsetOf activePeersSet establishedPeersSet)
+ . assert (Set.isSubsetOf activePeersSet establishedReadySet)
  . assert (Set.isSubsetOf establishedPeersSet knownPeersSet)
- . assert (Map.keysSet establishedStatus == establishedPeersSet)
+ . assert (EstablishedPeers.invariant establishedPeers)
    -- The localRootPeers and publicRootPeers must not overlap.
  . assert (Set.null (Set.intersection localRootPeersSet publicRootPeers))
     -- The localRootPeers are a subset of the knownPeers,
@@ -328,7 +328,8 @@ assertPeerSelectionState PeerSelectionState{..} =
   where
     localRootPeersSet   = Map.keysSet localRootPeers
     knownPeersSet       = Map.keysSet (KnownPeers.toMap knownPeers)
-    establishedPeersSet = Map.keysSet establishedPeers
+    establishedReadySet = Map.keysSet (EstablishedPeers.establishedReady establishedPeers)
+    establishedPeersSet = Map.keysSet (EstablishedPeers.allPeers establishedPeers)
     activePeersSet      = activePeers
     coldPeersSet        = knownPeersSet Set.\\ establishedPeersSet
     warmPeersSet        = establishedPeersSet Set.\\ activePeersSet

IntersectMBO/cardano-world

src/ouroboros-network/src/Ouroboros/Network/PeerSelection/Governor/ActivePeers.hs
@@ -17,6 +17,7 @@ import           Control.Concurrent.JobPool (Job(..))
 import           Control.Exception (SomeException, assert)
 
 import           Ouroboros.Network.PeerSelection.Types
+import qualified Ouroboros.Network.PeerSelection.EstablishedPeers as EstablishedPeers
 import           Ouroboros.Network.PeerSelection.KnownPeers (KnownPeerInfo(..))
 import qualified Ouroboros.Network.PeerSelection.KnownPeers as KnownPeers
 import           Ouroboros.Network.PeerSelection.Governor.Types
@@ -59,7 +60,7 @@ belowTarget actions
           -- The numPeersToPromote is positive due to the first guard.
       let availableToPromote :: Map peeraddr KnownPeerInfo
           availableToPromote = KnownPeers.toMap knownPeers
-                                `Map.intersection` establishedPeers
+                                `Map.intersection` EstablishedPeers.allPeers establishedPeers
                                 `Map.withoutKeys` activePeers
                                 `Map.withoutKeys` inProgressPromoteWarm
                                 `Map.withoutKeys` inProgressDemoteWarm
@@ -71,7 +72,7 @@ belowTarget actions
                              availableToPromote
                              numPeersToPromote
       let selectedToPromote' :: Map peeraddr peerconn
-          selectedToPromote' = establishedPeers
+          selectedToPromote' = EstablishedPeers.allPeers establishedPeers
                                  `Map.restrictKeys` selectedToPromote
       return $ \_now -> Decision {
         decisionTrace = TracePromoteWarmPeers
@@ -90,7 +91,7 @@ belowTarget actions
   = GuardedSkip Nothing
   where
     numEstablishedPeers, numActivePeers, numPromoteInProgress :: Int
-    numEstablishedPeers  = Map.size establishedPeers
+    numEstablishedPeers  = EstablishedPeers.size establishedPeers
     numActivePeers       = Set.size activePeers
     numPromoteInProgress = Set.size inProgressPromoteWarm
     numDemoteInProgress  = Set.size inProgressDemoteWarm
@@ -125,14 +126,15 @@ jobPromoteWarmPeer PeerSelectionActions{peerStateActions = PeerStateActions {act
       -- the responsibility of activatePeerConnection
       activatePeerConnection peerconn
       return $ Completion $ \st _now ->
-        assert (peeraddr `Map.member` establishedPeers st)
+        assert (peeraddr `EstablishedPeers.member` establishedPeers st)
         Decision {
           decisionTrace = TracePromoteWarmDone peeraddr,
           decisionState = st {
                             activePeers           = Set.insert peeraddr
                                                       (activePeers st),
-                            establishedStatus     = Map.insert peeraddr PeerHot
-                                                      (establishedStatus st),
+                            establishedPeers      = EstablishedPeers.updateStatus
+                                                      peeraddr PeerHot
+                                                      (establishedPeers st),
                             inProgressPromoteWarm = Set.delete peeraddr
                                                       (inProgressPromoteWarm st)
                           },
@@ -186,7 +188,7 @@ aboveTarget actions
                             availableToDemote
                             numPeersToDemote
       let selectedToDemote' :: Map peeraddr peerconn
-          selectedToDemote' = establishedPeers
+          selectedToDemote' = EstablishedPeers.allPeers establishedPeers
                                 `Map.restrictKeys` selectedToDemote
 
       return $ \_now -> Decision {
@@ -235,14 +237,14 @@ jobDemoteActivePeer PeerSelectionActions{peerStateActions = PeerStateActions {de
     job = do
       deactivatePeerConnection peerconn
       return $ Completion $ \st _now ->
-        assert (peeraddr `Map.member` establishedPeers st)
+        assert (peeraddr `EstablishedPeers.member` establishedPeers st)
         Decision {
           decisionTrace = TraceDemoteHotDone peeraddr,
           decisionState = st {
                             activePeers         = Set.delete peeraddr
                                                     (activePeers st),
-                            establishedStatus   = Map.insert peeraddr PeerWarm
-                                                    (establishedStatus st),
+                            establishedPeers    = EstablishedPeers.updateStatus peeraddr PeerWarm
+                                                    (establishedPeers st),
                             inProgressDemoteHot = Set.delete peeraddr
                                                     (inProgressDemoteHot st)
                           },

src/ouroboros-network/src/Ouroboros/Network/PeerSelection/Governor/Types.hs
@@ -19,6 +19,8 @@ import           Control.Monad.Class.MonadSTM
 import           Control.Monad.Class.MonadTime
 import           Control.Exception (assert, SomeException)
 
+import qualified Ouroboros.Network.PeerSelection.EstablishedPeers as EstablishedPeers
+import           Ouroboros.Network.PeerSelection.EstablishedPeers (EstablishedPeers)
 import qualified Ouroboros.Network.PeerSelection.KnownPeers as KnownPeers
 import           Ouroboros.Network.PeerSelection.KnownPeers (KnownPeers, KnownPeerInfo(..))
 import           Ouroboros.Network.PeerSelection.Types
@@ -217,8 +219,7 @@ data PeerSelectionState peeraddr peerconn = PeerSelectionState {
 
        -- |
        --
-       establishedPeers     :: !(Map peeraddr peerconn),
-       establishedStatus    :: !(Map peeraddr PeerStatus),
+       establishedPeers     :: !(EstablishedPeers peeraddr peerconn),
 
        -- |
        --
@@ -261,8 +262,7 @@ emptyPeerSelectionState =
       localRootPeers       = Map.empty,
       publicRootPeers      = Set.empty,
       knownPeers           = KnownPeers.empty,
-      establishedPeers     = Map.empty,
-      establishedStatus    = Map.empty,
+      establishedPeers     = EstablishedPeers.empty,
       activePeers          = Set.empty,
       publicRootBackoffs   = 0,
       publicRootRetryTime  = Time 0,
@@ -283,9 +283,9 @@ assertPeerSelectionState PeerSelectionState{..} =
 
     -- The activePeers is a subset of the establishedPeers
     -- which is a subset of the known peers
- . assert (Set.isSubsetOf activePeersSet establishedPeersSet)
+ . assert (Set.isSubsetOf activePeersSet establishedReadySet)
  . assert (Set.isSubsetOf establishedPeersSet knownPeersSet)
- . assert (Map.keysSet establishedStatus == establishedPeersSet)
+ . assert (EstablishedPeers.invariant establishedPeers)
    -- The localRootPeers and publicRootPeers must not overlap.
  . assert (Set.null (Set.intersection localRootPeersSet publicRootPeers))
     -- The localRootPeers are a subset of the knownPeers,
@@ -327,7 +327,8 @@ assertPeerSelectionState PeerSelectionState{..} =
   where
     localRootPeersSet   = Map.keysSet localRootPeers
     knownPeersSet       = Map.keysSet (KnownPeers.toMap knownPeers)
-    establishedPeersSet = Map.keysSet establishedPeers
+    establishedReadySet = Map.keysSet (EstablishedPeers.readyPeers establishedPeers)
+    establishedPeersSet = Map.keysSet (EstablishedPeers.allPeers establishedPeers)
     activePeersSet      = activePeers
     coldPeersSet        = knownPeersSet Set.\\ establishedPeersSet
     warmPeersSet        = establishedPeersSet Set.\\ activePeersSet