e148cee51155611a0355ee0b4a1641673a266314
Denis Shevchenko <denis.shevchenko@iohk.io>
2021-03-09 12:24:20 +0400

    Remove top Void. Request frequency.

--- src/ekg-forward/src/System/Metrics/Configuration.hs
+++ src/ekg-forward/src/System/Metrics/Configuration.hs
@@ -10,22 +10,32 @@ module System.Metrics.Configuration (
     AcceptorConfiguration (..)
   , ForwarderConfiguration (..)
   , HowToConnect (..)
+  , RequestFrequency (..)
+  , TimePeriod (..)
   ) where
 
 import           Data.Text (Text)
+import           Data.Word (Word16, Word64)
 
 type Host = Text
-type Port = Int
+type Port = Word16
 
 data HowToConnect
   = LocalPipe    !FilePath
   | RemoteSocket !Host !Port
   deriving (Eq, Show)
 
+data TimePeriod = Seconds | MilliSeconds
+  deriving (Eq, Show)
+
+data RequestFrequency = AskMetricsEvery !Word64 !TimePeriod
+  deriving (Eq, Show)
+
 data AcceptorConfiguration = AcceptorConfiguration
-  { listenToForwarder :: HowToConnect
+  { listenToForwarder :: !HowToConnect
+  , requestFrequency  :: !RequestFrequency
   } deriving (Eq, Show)
 
 data ForwarderConfiguration = ForwarderConfiguration
-  { connectToAcceptor :: HowToConnect
+  { connectToAcceptor :: !HowToConnect
   } deriving (Eq, Show)
