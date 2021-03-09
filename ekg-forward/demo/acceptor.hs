00b949dfba6f848cf1f789adccd70743eae4dc8a
Denis Shevchenko <denis.shevchenko@iohk.io>
2021-03-09 12:03:39 +0400

    Configuration init.

--- ekg-forward/demo/acceptor.hs
+++ ekg-forward/demo/acceptor.hs
@@ -1,7 +1,13 @@
 import           Data.Functor (void)
 
 import           System.Metrics.Acceptor (runEKGAcceptor)
+import           System.Metrics.Configuration (AcceptorConfiguration (..), HowToConnect (..))
 
 main :: IO ()
 main = do
-  void $ runEKGAcceptor "./demo-ekg-forward.sock"
+  void $ runEKGAcceptor config
+ where
+  config =
+    AcceptorConfiguration {
+      listenToForwarder = LocalPipe "./demo-ekg-forward.sock"
+    }
