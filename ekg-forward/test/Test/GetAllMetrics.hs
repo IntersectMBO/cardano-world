0d8144f2f43994a10c1605d90e6ea03234209644
Denis Shevchenko <denis.shevchenko@iohk.io>
2021-03-15 10:48:34 +0400

    Tests: legal stop of the protocol.

--- ekg-forward/test/Test/GetAllMetrics.hs
+++ ekg-forward/test/Test/GetAllMetrics.hs
@@ -5,8 +5,8 @@ module Test.GetAllMetrics
 
 import Test.Hspec
 
-import           Control.Concurrent (forkIO, killThread, threadDelay)
-import           Data.IORef (newIORef)
+import           Control.Concurrent (forkIO, threadDelay)
+import           Data.IORef (atomicModifyIORef', newIORef)
 import qualified System.Metrics as EKG
 import qualified System.Metrics.Gauge as G
 import qualified System.Metrics.Label as L
@@ -31,8 +31,8 @@ getAllMetrics endpoint = do
 
   let acceptorConfig = mkAcceptorConfig endpoint weAreDone GetAllMetrics
       forwarderConfig = mkForwarderConfig endpoint
-  
-  acceptorThr <- forkIO $ runEKGAcceptor acceptorConfig acceptorStore
+
+  _acceptorThr <- forkIO $ runEKGAcceptor acceptorConfig acceptorStore
 
   EKG.createGauge   "test1.gauge.1" forwarderStore >>= flip G.set 123
   EKG.createGauge   "test1.gauge.2" forwarderStore >>= flip G.set 456
@@ -40,12 +40,13 @@ getAllMetrics endpoint = do
   EKG.createLabel   "test1.label.2" forwarderStore >>= flip L.set "TestLabel_2"
   EKG.createCounter "test1.cntr.1"  forwarderStore >>= flip C.add 10
 
-  forwarderThr <- forkIO $ runEKGForwarder forwarderConfig forwarderStore
+  _forwarderThr <- forkIO $ runEKGForwarder forwarderConfig forwarderStore
 
   threadDelay 2000000
 
-  killThread forwarderThr
-  killThread acceptorThr
+  atomicModifyIORef' weAreDone (const (True, ()))
+
+  threadDelay 1000000
 
   forwarderMetrics <- EKG.sampleAll forwarderStore
   acceptorMetrics  <- EKG.sampleAll acceptorStore
