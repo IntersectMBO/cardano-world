qbsoftware/cardano-node

CAD-2696 Add TestEnableDevelopmentNetworkProtocols node config option
This config option governs whether to limit the negotiated node-to-node
and node-to-client protocol versions to that of the latest official
release.

It defaults to 'False'. For example, the QA Team must set this to 'True'
in order to exercise a feature that is (partially) developed but not
scheduled for activation in the next release.

Co-authored-by: Duncan Coutts <duncan@well-typed.com>

cardano-node/test/Test/Cardano/Node/POM.hs
@@ -49,6 +49,7 @@ testPartialYamlConfig =
     , pncSocketPath = Last Nothing
     , pncDiffusionMode = Last Nothing
     , pncSnapshotInterval = mempty
+    , pncTestInDevelopmentVersions = Last Nothing
     , pncMaxConcurrencyBulkSync = Last Nothing
     , pncMaxConcurrencyDeadline = Last Nothing
     , pncLoggingSwitch = Last $ Just True
@@ -80,6 +81,7 @@ testPartialCliConfig =
     , pncSocketPath   = mempty
     , pncDiffusionMode = mempty
     , pncSnapshotInterval = Last . Just . RequestedSnapshotInterval $ secondsToDiffTime 100
+    , pncTestInDevelopmentVersions = Last $ Just True
     , pncProtocolFiles = Last . Just $ ProtocolFilepaths Nothing Nothing Nothing Nothing Nothing Nothing
     , pncValidateDB = Last $ Just True
     , pncShutdownIPC = Last $ Just Nothing
@@ -112,6 +114,7 @@ expectedConfig =
     , ncSocketPath = Nothing
     , ncDiffusionMode = InitiatorAndResponderDiffusionMode
     , ncSnapshotInterval = RequestedSnapshotInterval $ secondsToDiffTime 100
+    , ncTestInDevelopmentVersions = True
     , ncMaxConcurrencyBulkSync = Nothing
     , ncMaxConcurrencyDeadline = Nothing
     , ncLoggingSwitch = True

IntersectMBO/cardano-world

cardano-node/test/Test/Cardano/Node/POM.hs
@@ -49,6 +49,7 @@ testPartialYamlConfig =
     , pncSocketPath = Last Nothing
     , pncDiffusionMode = Last Nothing
     , pncSnapshotInterval = mempty
+    , pncTestEnableDevelopmentNetworkProtocols = Last Nothing
     , pncMaxConcurrencyBulkSync = Last Nothing
     , pncMaxConcurrencyDeadline = Last Nothing
     , pncLoggingSwitch = Last $ Just True
@@ -80,6 +81,7 @@ testPartialCliConfig =
     , pncSocketPath   = mempty
     , pncDiffusionMode = mempty
     , pncSnapshotInterval = Last . Just . RequestedSnapshotInterval $ secondsToDiffTime 100
+    , pncTestEnableDevelopmentNetworkProtocols = Last $ Just True
     , pncProtocolFiles = Last . Just $ ProtocolFilepaths Nothing Nothing Nothing Nothing Nothing Nothing
     , pncValidateDB = Last $ Just True
     , pncShutdownIPC = Last $ Just Nothing
@@ -112,6 +114,7 @@ expectedConfig =
     , ncSocketPath = Nothing
     , ncDiffusionMode = InitiatorAndResponderDiffusionMode
     , ncSnapshotInterval = RequestedSnapshotInterval $ secondsToDiffTime 100
+    , ncTestEnableDevelopmentNetworkProtocols = True
     , ncMaxConcurrencyBulkSync = Nothing
     , ncMaxConcurrencyDeadline = Nothing
     , ncLoggingSwitch = True