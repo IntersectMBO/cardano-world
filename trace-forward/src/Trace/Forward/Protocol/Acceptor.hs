MELD-labs/cardano-node

CAD-3243: trace-forward library.

trace-forward/src/Trace/Forward/Protocol/Acceptor.hs
@@ -1,0 +1,78 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE RankNTypes #-}
+
+-- | A view of the trace forwarding/accepting protocol
+--   from the point of view of the client.
+--
+-- For execution, a conversion into the typed protocol is provided.
+--
+module Trace.Forward.Protocol.Acceptor
+  ( TraceAcceptor(..)
+  , traceAcceptorPeer
+  ) where
+
+import           Network.TypedProtocol.Core (Peer (..), PeerHasAgency (..),
+                                             PeerRole (..))
+
+import           Trace.Forward.Protocol.Type
+
+data TraceAcceptor lo m a where
+  SendMsgNodeInfoRequest
+    :: (NodeInfo -> m (TraceAcceptor lo m a))
+    -> TraceAcceptor lo m a
+
+  SendMsgRequest
+    :: TokBlockingStyle blocking
+    -> Request
+    -> (BlockingReplyList blocking lo -> m (TraceAcceptor lo m a))
+    -> TraceAcceptor lo m a
+
+  SendMsgDone
+    :: m a
+    -> TraceAcceptor lo m a
+
+-- | Interpret a particular action sequence into the client side of the protocol.
+--
+traceAcceptorPeer
+  :: Monad m
+  => TraceAcceptor lo m a
+  -> Peer (TraceForward lo) 'AsClient 'StIdle m a
+traceAcceptorPeer = \case
+  SendMsgNodeInfoRequest next ->
+    -- Send our message (request for node's info from the forwarder).
+    Yield (ClientAgency TokIdle) MsgNodeInfoRequest $
+      -- We're now into the 'StNodeInfoBusy' state, and now we'll wait for
+      -- a reply from the forwarder.
+      Await (ServerAgency TokNodeInfoBusy) $ \(MsgNodeInfoReply reply) ->
+        Effect $
+          traceAcceptorPeer <$> next reply
+
+  SendMsgRequest TokBlocking request next ->
+    -- Send our message (request for new 'TraceObject's from the forwarder).
+    Yield (ClientAgency TokIdle) (MsgRequest TokBlocking request) $
+      -- We're now into the 'StBusy' state, and now we'll wait for a reply
+      -- from the forwarder.
+      Await (ServerAgency (TokBusy TokBlocking)) $ \(MsgReply reply) ->
+        Effect $
+          traceAcceptorPeer <$> next reply
+
+  SendMsgRequest TokNonBlocking request next ->
+    -- Send our message (request for new 'TraceObject's from the forwarder).
+    Yield (ClientAgency TokIdle) (MsgRequest TokNonBlocking request) $
+      -- We're now into the 'StBusy' state, and now we'll wait for a reply
+      -- from the forwarder. It is assuming that the forwarder will reply
+      -- immediately (even there are no 'TraceObject's).
+      Await (ServerAgency (TokBusy TokNonBlocking)) $ \(MsgReply reply) ->
+        Effect $
+          traceAcceptorPeer <$> next reply
+
+  SendMsgDone getResult ->
+    -- We do an actual transition using 'yield', to go from the 'StIdle' to
+    -- 'StDone' state. Once in the 'StDone' state we can actually stop using
+    -- 'done', with a return value.
+    Effect $
+      Yield (ClientAgency TokIdle) MsgDone . Done TokDone
+        <$> getResult

trace-forward/src/Trace/Forward/Protocol/Codec.hs
@@ -1,0 +1,128 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Trace.Forward.Protocol.Codec (
+  codecTraceForward
+  ) where
+
+import qualified Codec.CBOR.Decoding as CBOR
+import qualified Codec.CBOR.Encoding as CBOR
+import           Codec.CBOR.Read (DeserialiseFailure)
+import           Control.Monad.Class.MonadST (MonadST)
+import qualified Data.ByteString.Lazy as LBS
+import qualified Data.List.NonEmpty as NE
+import           Text.Printf (printf)
+import           Ouroboros.Network.Codec (Codec, PeerHasAgency (..),
+                                          PeerRole (..), SomeMessage (..),
+                                          mkCodecCborLazyBS)
+
+import           Trace.Forward.Protocol.Type
+
+codecTraceForward
+  :: forall lo m.
+     MonadST m
+  => (Request -> CBOR.Encoding)           -- ^ Encoder for 'Request'.
+  -> (forall s . CBOR.Decoder s Request)  -- ^ Decoder for 'Request'.
+  -> (NodeInfo -> CBOR.Encoding)          -- ^ Encoder for reply with node's info.
+  -> (forall s . CBOR.Decoder s NodeInfo) -- ^ Decoder for reply with node's info.
+  -> ([lo] -> CBOR.Encoding)              -- ^ Encoder for reply with list of 'TraceObject's.
+  -> (forall s . CBOR.Decoder s [lo])     -- ^ Decoder for reply with list of 'TraceObject's.
+  -> Codec (TraceForward lo)
+           DeserialiseFailure m LBS.ByteString
+codecTraceForward encodeRequest   decodeRequest
+                  encodeNIReply   decodeNIReply
+                  encodeReplyList decodeReplyList =
+  mkCodecCborLazyBS encode decode
+ where
+  -- Encode messages.
+  encode
+    :: forall (pr  :: PeerRole)
+              (st  :: TraceForward lo)
+              (st' :: TraceForward lo).
+       PeerHasAgency pr st
+    -> Message (TraceForward lo) st st'
+    -> CBOR.Encoding
+
+  encode (ClientAgency TokIdle) MsgNodeInfoRequest =
+         CBOR.encodeListLen 1
+      <> CBOR.encodeWord 0
+
+  encode (ClientAgency TokIdle) (MsgRequest blocking request) =
+         CBOR.encodeListLen 3
+      <> CBOR.encodeWord 1
+      <> CBOR.encodeBool (case blocking of
+                            TokBlocking    -> True
+                            TokNonBlocking -> False)
+      <> encodeRequest request
+
+  encode (ClientAgency TokIdle) MsgDone =
+         CBOR.encodeListLen 1
+      <> CBOR.encodeWord 2
+
+  encode (ServerAgency TokNodeInfoBusy) (MsgNodeInfoReply reply) =
+         CBOR.encodeListLen 2
+      <> CBOR.encodeWord 3
+      <> encodeNIReply reply
+
+  encode (ServerAgency (TokBusy _)) (MsgReply reply) =
+         CBOR.encodeListLen 2
+      <> CBOR.encodeWord 4
+      <> encodeReplyList replyList
+   where
+    replyList =
+      case reply of
+        BlockingReply    los -> NE.toList los
+        NonBlockingReply los -> los
+
+  -- Decode messages
+  decode
+    :: forall (pr :: PeerRole)
+              (st :: TraceForward lo) s.
+       PeerHasAgency pr st
+    -> CBOR.Decoder s (SomeMessage st)
+  decode stok = do
+    len <- CBOR.decodeListLen
+    key <- CBOR.decodeWord
+    case (key, len, stok) of
+      (0, 1, ClientAgency TokIdle) ->
+        return $ SomeMessage MsgNodeInfoRequest
+
+      (1, 3, ClientAgency TokIdle) -> do
+        blocking <- CBOR.decodeBool
+        request <- decodeRequest
+        return $!
+          if blocking then
+            SomeMessage $ MsgRequest TokBlocking request
+          else
+            SomeMessage $ MsgRequest TokNonBlocking request
+
+      (2, 1, ClientAgency TokIdle) ->
+        return $ SomeMessage MsgDone
+
+      (3, 2, ServerAgency TokNodeInfoBusy) ->
+        SomeMessage . MsgNodeInfoReply <$> decodeNIReply
+
+      (4, 2, ServerAgency (TokBusy blocking)) -> do
+        replyList <- decodeReplyList
+        case (blocking, replyList) of
+          (TokBlocking, x:xs) ->
+            return $ SomeMessage (MsgReply (BlockingReply (x NE.:| xs)))
+
+          (TokNonBlocking, los) ->
+            return $ SomeMessage (MsgReply (NonBlockingReply los))
+
+          (TokBlocking, []) ->
+            fail "codecTraceForward: MsgReply: empty list not permitted"
+
+      -- Failures per protocol state
+      (_, _, ClientAgency TokIdle) ->
+        fail (printf "codecTraceForward (%s) unexpected key (%d, %d)" (show stok) key len)
+      (_, _, ServerAgency TokNodeInfoBusy) ->
+        fail (printf "codecTraceForward (%s) unexpected key (%d, %d)" (show stok) key len)
+      (_, _, ServerAgency (TokBusy TokBlocking)) ->
+        fail (printf "codecTraceForward (%s) unexpected key (%d, %d)" (show stok) key len)
+      (_, _, ServerAgency (TokBusy TokNonBlocking)) ->
+        fail (printf "codecTraceForward (%s) unexpected key (%d, %d)" (show stok) key len)

IntersectMBO/cardano-world

trace-forward/src/Trace/Forward/Protocol/Acceptor.hs
@@ -1,0 +1,78 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE RankNTypes #-}
+
+-- | A view of the trace forwarding/accepting protocol
+--   from the point of view of the client.
+--
+-- For execution, a conversion into the typed protocol is provided.
+--
+module Trace.Forward.Protocol.Acceptor
+  ( TraceAcceptor(..)
+  , traceAcceptorPeer
+  ) where
+
+import           Network.TypedProtocol.Core (Peer (..), PeerHasAgency (..),
+                                             PeerRole (..))
+
+import           Trace.Forward.Protocol.Type
+
+data TraceAcceptor lo m a where
+  SendMsgNodeInfoRequest
+    :: (NodeInfo -> m (TraceAcceptor lo m a))
+    -> TraceAcceptor lo m a
+
+  SendMsgTraceObjectsRequest
+    :: TokBlockingStyle blocking
+    -> NumberOfTraceObjects
+    -> (BlockingReplyList blocking lo -> m (TraceAcceptor lo m a))
+    -> TraceAcceptor lo m a
+
+  SendMsgDone
+    :: m a
+    -> TraceAcceptor lo m a
+
+-- | Interpret a particular action sequence into the client side of the protocol.
+--
+traceAcceptorPeer
+  :: Monad m
+  => TraceAcceptor lo m a
+  -> Peer (TraceForward lo) 'AsClient 'StIdle m a
+traceAcceptorPeer = \case
+  SendMsgNodeInfoRequest next ->
+    -- Send our message (request for node's info from the forwarder).
+    Yield (ClientAgency TokIdle) MsgNodeInfoRequest $
+      -- We're now into the 'StNodeInfoBusy' state, and now we'll wait for
+      -- a reply from the forwarder.
+      Await (ServerAgency TokNodeInfoBusy) $ \(MsgNodeInfoReply reply) ->
+        Effect $
+          traceAcceptorPeer <$> next reply
+
+  SendMsgTraceObjectsRequest TokBlocking request next ->
+    -- Send our message (request for new 'TraceObject's from the forwarder).
+    Yield (ClientAgency TokIdle) (MsgTraceObjectsRequest TokBlocking request) $
+      -- We're now into the 'StBusy' state, and now we'll wait for a reply
+      -- from the forwarder.
+      Await (ServerAgency (TokBusy TokBlocking)) $ \(MsgTraceObjectsReply reply) ->
+        Effect $
+          traceAcceptorPeer <$> next reply
+
+  SendMsgTraceObjectsRequest TokNonBlocking request next ->
+    -- Send our message (request for new 'TraceObject's from the forwarder).
+    Yield (ClientAgency TokIdle) (MsgTraceObjectsRequest TokNonBlocking request) $
+      -- We're now into the 'StBusy' state, and now we'll wait for a reply
+      -- from the forwarder. It is assuming that the forwarder will reply
+      -- immediately (even there are no 'TraceObject's).
+      Await (ServerAgency (TokBusy TokNonBlocking)) $ \(MsgTraceObjectsReply reply) ->
+        Effect $
+          traceAcceptorPeer <$> next reply
+
+  SendMsgDone getResult ->
+    -- We do an actual transition using 'yield', to go from the 'StIdle' to
+    -- 'StDone' state. Once in the 'StDone' state we can actually stop using
+    -- 'done', with a return value.
+    Effect $
+      Yield (ClientAgency TokIdle) MsgDone . Done TokDone
+        <$> getResult

trace-forward/src/Trace/Forward/Protocol/Codec.hs
@@ -1,0 +1,128 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Trace.Forward.Protocol.Codec (
+  codecTraceForward
+  ) where
+
+import qualified Codec.CBOR.Decoding as CBOR
+import qualified Codec.CBOR.Encoding as CBOR
+import           Codec.CBOR.Read (DeserialiseFailure)
+import           Control.Monad.Class.MonadST (MonadST)
+import qualified Data.ByteString.Lazy as LBS
+import qualified Data.List.NonEmpty as NE
+import           Text.Printf (printf)
+import           Ouroboros.Network.Codec (Codec, PeerHasAgency (..),
+                                          PeerRole (..), SomeMessage (..),
+                                          mkCodecCborLazyBS)
+
+import           Trace.Forward.Protocol.Type
+
+codecTraceForward
+  :: forall lo m.
+     MonadST m
+  => (NumberOfTraceObjects -> CBOR.Encoding)          -- ^ Encoder for 'Request'.
+  -> (forall s . CBOR.Decoder s NumberOfTraceObjects) -- ^ Decoder for 'Request'.
+  -> (NodeInfo -> CBOR.Encoding)                      -- ^ Encoder for reply with node's info.
+  -> (forall s . CBOR.Decoder s NodeInfo)             -- ^ Decoder for reply with node's info.
+  -> ([lo] -> CBOR.Encoding)                          -- ^ Encoder for reply with list of 'TraceObject's.
+  -> (forall s . CBOR.Decoder s [lo])                 -- ^ Decoder for reply with list of 'TraceObject's.
+  -> Codec (TraceForward lo)
+           DeserialiseFailure m LBS.ByteString
+codecTraceForward encodeRequest   decodeRequest
+                  encodeNIReply   decodeNIReply
+                  encodeReplyList decodeReplyList =
+  mkCodecCborLazyBS encode decode
+ where
+  -- Encode messages.
+  encode
+    :: forall (pr  :: PeerRole)
+              (st  :: TraceForward lo)
+              (st' :: TraceForward lo).
+       PeerHasAgency pr st
+    -> Message (TraceForward lo) st st'
+    -> CBOR.Encoding
+
+  encode (ClientAgency TokIdle) MsgNodeInfoRequest =
+         CBOR.encodeListLen 1
+      <> CBOR.encodeWord 0
+
+  encode (ClientAgency TokIdle) (MsgTraceObjectsRequest blocking request) =
+         CBOR.encodeListLen 3
+      <> CBOR.encodeWord 1
+      <> CBOR.encodeBool (case blocking of
+                            TokBlocking    -> True
+                            TokNonBlocking -> False)
+      <> encodeRequest request
+
+  encode (ClientAgency TokIdle) MsgDone =
+         CBOR.encodeListLen 1
+      <> CBOR.encodeWord 2
+
+  encode (ServerAgency TokNodeInfoBusy) (MsgNodeInfoReply reply) =
+         CBOR.encodeListLen 2
+      <> CBOR.encodeWord 3
+      <> encodeNIReply reply
+
+  encode (ServerAgency (TokBusy _)) (MsgTraceObjectsReply reply) =
+         CBOR.encodeListLen 2
+      <> CBOR.encodeWord 4
+      <> encodeReplyList replyList
+   where
+    replyList =
+      case reply of
+        BlockingReply    los -> NE.toList los
+        NonBlockingReply los -> los
+
+  -- Decode messages
+  decode
+    :: forall (pr :: PeerRole)
+              (st :: TraceForward lo) s.
+       PeerHasAgency pr st
+    -> CBOR.Decoder s (SomeMessage st)
+  decode stok = do
+    len <- CBOR.decodeListLen
+    key <- CBOR.decodeWord
+    case (key, len, stok) of
+      (0, 1, ClientAgency TokIdle) ->
+        return $ SomeMessage MsgNodeInfoRequest
+
+      (1, 3, ClientAgency TokIdle) -> do
+        blocking <- CBOR.decodeBool
+        request <- decodeRequest
+        return $!
+          if blocking then
+            SomeMessage $ MsgTraceObjectsRequest TokBlocking request
+          else
+            SomeMessage $ MsgTraceObjectsRequest TokNonBlocking request
+
+      (2, 1, ClientAgency TokIdle) ->
+        return $ SomeMessage MsgDone
+
+      (3, 2, ServerAgency TokNodeInfoBusy) ->
+        SomeMessage . MsgNodeInfoReply <$> decodeNIReply
+
+      (4, 2, ServerAgency (TokBusy blocking)) -> do
+        replyList <- decodeReplyList
+        case (blocking, replyList) of
+          (TokBlocking, x:xs) ->
+            return $ SomeMessage (MsgTraceObjectsReply (BlockingReply (x NE.:| xs)))
+
+          (TokNonBlocking, los) ->
+            return $ SomeMessage (MsgTraceObjectsReply (NonBlockingReply los))
+
+          (TokBlocking, []) ->
+            fail "codecTraceForward: MsgTraceObjectsReply: empty list not permitted"
+
+      -- Failures per protocol state
+      (_, _, ClientAgency TokIdle) ->
+        fail (printf "codecTraceForward (%s) unexpected key (%d, %d)" (show stok) key len)
+      (_, _, ServerAgency TokNodeInfoBusy) ->
+        fail (printf "codecTraceForward (%s) unexpected key (%d, %d)" (show stok) key len)
+      (_, _, ServerAgency (TokBusy TokBlocking)) ->
+        fail (printf "codecTraceForward (%s) unexpected key (%d, %d)" (show stok) key len)
+      (_, _, ServerAgency (TokBusy TokNonBlocking)) ->
+        fail (printf "codecTraceForward (%s) unexpected key (%d, %d)" (show stok) key len)