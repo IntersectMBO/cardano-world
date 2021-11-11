IntersectMBO/ouroboros-network

p2p-governor: peer metrics using Tracer

ouroboros-consensus/src/Ouroboros/Consensus/Network/NodeToNode.hs
@@ -58,8 +58,8 @@ import           Ouroboros.Network.KeepAlive
 import           Ouroboros.Network.Mux
 import           Ouroboros.Network.NodeToNode
 import           Ouroboros.Network.PeerSelection.PeerMetric.Type
-                     (ReportFetchedMetricsSTM, ReportHeaderMetricsSTM,
-                     ReportPeerMetrics (..))
+                     (FetchedMetricsTracer, HeaderMetricsTracer,
+                     ReportPeerMetrics (..), WithPeer (..))
 import           Ouroboros.Network.Protocol.BlockFetch.Codec
 import           Ouroboros.Network.Protocol.BlockFetch.Server (BlockFetchServer,
                      blockFetchServerPeer)
@@ -114,7 +114,7 @@ data Handlers m peer blk = Handlers {
         :: peer
         -> NodeToNodeVersion
         -> ControlMessageSTM m
-        -> ReportHeaderMetricsSTM m
+        -> HeaderMetricsTracer m
         -> StrictTVar m (AnchoredFragment (Header blk))
         -> ChainSyncClientPipelined (Header blk) (Point blk) (Tip blk) m ChainSyncClientResult
         -- TODO: we should consider either bundling these context parameters
@@ -131,7 +131,7 @@ data Handlers m peer blk = Handlers {
     , hBlockFetchClient
         :: NodeToNodeVersion
         -> ControlMessageSTM m
-        -> ReportFetchedMetricsSTM m
+        -> FetchedMetricsTracer m
         -> BlockFetchClient (Header blk) blk m ()
 
     , hBlockFetchServer
@@ -494,7 +494,8 @@ mkApps kernel Tracers {..} mkCodecs genChainSyncTimeout ReportPeerMetrics {..} H
                   channel
                   $ chainSyncClientPeerPipelined
                   $ hChainSyncClient them version controlMessageSTM
-                      (reportHeader them) varCandidate
+                      (WithPeer them `contramap` reportHeader)
+                      varCandidate
               return ((), trailing)
 
     aChainSyncServer
@@ -533,7 +534,8 @@ mkApps kernel Tracers {..} mkCodecs genChainSyncTimeout ReportPeerMetrics {..} H
           (byteLimitsBlockFetch (const 0)) -- TODO: Real Bytelimits, see #1727
           timeLimitsBlockFetch
           channel
-          $ hBlockFetchClient version controlMessageSTM (reportFetch them) clientCtx
+          $ hBlockFetchClient version controlMessageSTM
+                              (WithPeer them `contramap` reportFetch) clientCtx
 
     aBlockFetchServer
       :: NodeToNodeVersion

IntersectMBO/cardano-world

src/ouroboros-consensus/src/Ouroboros/Consensus/Network/NodeToNode.hs
@@ -66,7 +66,7 @@ import           Ouroboros.Network.KeepAlive
 import           Ouroboros.Network.Mux
 import           Ouroboros.Network.NodeToNode
 import           Ouroboros.Network.PeerSelection.PeerMetric.Type
-                     (ReportFetchedMetricsSTM, ReportHeaderMetricsSTM,
+                     (FetchedMetricsTracer, HeaderMetricsTracer,
                      ReportPeerMetrics (..))
 import           Ouroboros.Network.Protocol.BlockFetch.Codec
 import           Ouroboros.Network.Protocol.BlockFetch.Server (BlockFetchServer,
@@ -122,7 +122,7 @@ data Handlers m peer blk = Handlers {
         :: peer
         -> NodeToNodeVersion
         -> ControlMessageSTM m
-        -> ReportHeaderMetricsSTM m
+        -> HeaderMetricsTracer m
         -> StrictTVar m (AnchoredFragment (Header blk))
         -> ChainSyncClientPipelined (Header blk) (Point blk) (Tip blk) m ChainSyncClientResult
         -- TODO: we should consider either bundling these context parameters
@@ -139,7 +139,7 @@ data Handlers m peer blk = Handlers {
     , hBlockFetchClient
         :: NodeToNodeVersion
         -> ControlMessageSTM m
-        -> ReportFetchedMetricsSTM m
+        -> FetchedMetricsTracer m
         -> BlockFetchClient (Header blk) blk m ()
 
     , hBlockFetchServer
@@ -559,7 +559,8 @@ mkApps kernel Tracers {..} mkCodecs ByteLimits {..} genChainSyncTimeout ReportPe
                   channel
                   $ chainSyncClientPeerPipelined
                   $ hChainSyncClient them version controlMessageSTM
-                      (reportHeader them) varCandidate
+                      (TraceLabelPeer them `contramap` reportHeader)
+                      varCandidate
               return ((), trailing)
 
     aChainSyncServer
@@ -598,7 +599,8 @@ mkApps kernel Tracers {..} mkCodecs ByteLimits {..} genChainSyncTimeout ReportPe
           blBlockFetch
           timeLimitsBlockFetch
           channel
-          $ hBlockFetchClient version controlMessageSTM (reportFetch them) clientCtx
+          $ hBlockFetchClient version controlMessageSTM
+                              (TraceLabelPeer them `contramap` reportFetch) clientCtx
 
     aBlockFetchServer
       :: NodeToNodeVersion