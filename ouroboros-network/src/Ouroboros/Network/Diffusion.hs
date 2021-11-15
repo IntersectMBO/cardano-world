IntersectMBO/ouroboros-network

diffusion: run diffusion in an abstract monad
Also introduce `Interfaces` record which provides all the interfaces
(e.g. snockets, dns resolver, etc) needed to run data diffusion.

ouroboros-network/src/Ouroboros/Network/Diffusion.hs
@@ -23,10 +23,12 @@ module Ouroboros.Network.Diffusion
   where
 
 import           Data.Functor (void)
+import           Control.Exception (IOException)
+
+import           Network.Socket (Socket)
 
 import           Ouroboros.Network.NodeToNode
                   ( RemoteAddress
-                  , Socket
                   , NodeToNodeVersionData
                   , NodeToNodeVersion
                   )
@@ -53,6 +55,7 @@ data ExtraTracers (p2p :: P2P) where
     :: P2P.TracersExtra
            RemoteAddress  NodeToNodeVersion   NodeToNodeVersionData
            LocalAddress   NodeToClientVersion NodeToClientVersionData
+           IOException IO
     -> ExtraTracers 'P2P
 
   NonP2PTracers
@@ -62,26 +65,26 @@ data ExtraTracers (p2p :: P2P) where
 
 -- | Diffusion arguments which depend on p2p mode.
 --
-data ExtraArguments (p2p :: P2P) where
+data ExtraArguments (p2p :: P2P) m where
   P2PArguments
-    :: P2P.ArgumentsExtra
-    -> ExtraArguments 'P2P
+    :: P2P.ArgumentsExtra m
+    -> ExtraArguments 'P2P m
 
   NonP2PArguments
     :: NonP2P.ArgumentsExtra
-    -> ExtraArguments 'NonP2P
+    -> ExtraArguments 'NonP2P m
 
 
 -- | Application data which depend on p2p mode.
 --
-data ExtraApplications (p2p :: P2P) n2nAddr where
+data ExtraApplications (p2p :: P2P) n2nAddr m where
   P2PApplications
-    :: P2P.ApplicationsExtra ntnAddr
-    -> ExtraApplications 'P2P ntnAddr
+    :: P2P.ApplicationsExtra ntnAddr m
+    -> ExtraApplications 'P2P ntnAddr m
 
   NonP2PApplications
     :: NonP2P.ApplicationsExtra
-    -> ExtraApplications 'NonP2P ntnAddr
+    -> ExtraApplications 'NonP2P ntnAddr m
 
 
 -- | Run data diffusion in either 'P2P' or 'NonP2P' mode.
@@ -90,15 +93,17 @@ run :: forall (p2p :: P2P).
        Tracers
          RemoteAddress NodeToNodeVersion
          LocalAddress  NodeToClientVersion
+         IO
     -> ExtraTracers p2p
     -> Arguments
          Socket      RemoteAddress 
          LocalSocket LocalAddress
-    -> ExtraArguments p2p
+    -> ExtraArguments p2p IO
     -> Applications
          RemoteAddress  NodeToNodeVersion   NodeToNodeVersionData
          LocalAddress   NodeToClientVersion NodeToClientVersionData
-    -> ExtraApplications p2p RemoteAddress
+         IO
+    -> ExtraApplications p2p RemoteAddress IO
     -> IO ()
 run tracers (P2PTracers tracersExtra)
             args (P2PArguments argsExtra)

ouroboros-network/src/Ouroboros/Network/PeerSelection/Simple.hs
@@ -14,10 +14,11 @@ module Ouroboros.Network.PeerSelection.Simple
 
 import           Data.Foldable (toList)
 import           Control.Monad.Class.MonadAsync
+import           Control.Monad.Class.MonadThrow
 import           Control.Monad.Class.MonadTime
+import           Control.Monad.Class.MonadTimer
 import           Control.Monad.Class.MonadSTM.Strict
 import           Control.Tracer (Tracer)
-import           Control.Exception (IOException)
 
 import           Data.Map (Map)
 import           Data.Set (Set)
@@ -33,28 +34,36 @@ import           Ouroboros.Network.PeerSelection.RootPeersDNS
 
 
 withPeerSelectionActions
-  :: forall peeraddr peerconn a.
-     Ord peeraddr
-  => Tracer IO (TraceLocalRootPeers peeraddr IOException)
-  -> Tracer IO TracePublicRootPeers
+  :: forall peeraddr peerconn resolver exception m a.
+     ( MonadAsync m
+     , MonadDelay m
+     , MonadThrow m
+     , Ord peeraddr
+     , Exception exception
+     , Eq (Async m Void)
+     )
+  => Tracer m (TraceLocalRootPeers peeraddr exception)
+  -> Tracer m TracePublicRootPeers
   -> (IP -> Socket.PortNumber -> peeraddr)
-  -> STM IO PeerSelectionTargets
-  -> STM IO [(Int, Map RelayAccessPoint PeerAdvertise)]
+  -> DNSActions resolver exception m
+  -> STM m PeerSelectionTargets
+  -> STM m [(Int, Map RelayAccessPoint PeerAdvertise)]
   -- ^ local root peers
-  -> STM IO [RelayAccessPoint]
+  -> STM m [RelayAccessPoint]
   -- ^ public root peers
-  -> PeerStateActions peeraddr peerconn IO
-  -> (NumberOfPeers -> IO (Maybe (Set peeraddr, DiffTime)))
-  -> (Maybe (Async IO Void)
-      -> PeerSelectionActions peeraddr peerconn IO
-      -> IO a)
+  -> PeerStateActions peeraddr peerconn m
+  -> (NumberOfPeers -> m (Maybe (Set peeraddr, DiffTime)))
+  -> (Maybe (Async m Void)
+      -> PeerSelectionActions peeraddr peerconn m
+      -> m a)
   -- ^ continuation, recieves a handle to the local roots peer provider thread
   -- (only if local root peers where non-empty).
-  -> IO a
+  -> m a
 withPeerSelectionActions
   localRootTracer
   publicRootTracer
   toPeerAddr
+  dnsActions
   readTargets
   readLocalRootPeers
   readPublicRootPeers
@@ -65,7 +74,7 @@ withPeerSelectionActions
     let peerSelectionActions = PeerSelectionActions {
             readPeerSelectionTargets = readTargets,
             readLocalRootPeers = toList <$> readTVar localRootsVar,
-            requestPublicRootPeers = requestPublicRootPeers ioDNSActions,
+            requestPublicRootPeers = requestLedgerPeers,
             requestPeerGossip = \_ -> pure [],
             peerStateActions
           }
@@ -74,7 +83,7 @@ withPeerSelectionActions
         localRootTracer
         toPeerAddr
         DNS.defaultResolvConf
-        ioDNSActions
+        dnsActions
         readLocalRootPeers
         localRootsVar)
       (\thread -> k (Just thread) peerSelectionActions)
@@ -82,20 +91,18 @@ withPeerSelectionActions
     -- We first try to get poublic root peers from the ledger, but if it fails
     -- (for example because the node hasn't synced far enough) we fall back
     -- to using the manually configured bootstrap root peers.
-    requestPublicRootPeers :: DNSActions DNS.Resolver IOException IO
-                           -> Int -> IO (Set peeraddr, DiffTime)
-    requestPublicRootPeers dnsActions n = do
-      peers_m <- getLedgerPeers (NumberOfPeers $ fromIntegral n)
-      case peers_m of
-           Nothing    -> requestConfiguredRootPeers dnsActions n
-           Just peers -> return peers
+    requestLedgerPeers :: Int -> m (Set peeraddr, DiffTime)
+    requestLedgerPeers n = do
+        peers_m <- getLedgerPeers $ NumberOfPeers $ fromIntegral n
+        case peers_m of
+             Nothing    -> requestPublicRootPeers n
+             Just peers -> return peers
 
     -- For each call we re-initialise the dns library which forces reading
     -- `/etc/resolv.conf`:
     -- https://github.com/input-output-hk/cardano-node/issues/731
-    requestConfiguredRootPeers :: DNSActions DNS.Resolver IOException IO
-                               -> Int -> IO (Set peeraddr, DiffTime)
-    requestConfiguredRootPeers dnsActions n =
+    requestPublicRootPeers :: Int -> m (Set peeraddr, DiffTime)
+    requestPublicRootPeers n =
       publicRootPeersProvider publicRootTracer
                               toPeerAddr
                               DNS.defaultResolvConf

IntersectMBO/cardano-world

ouroboros-network/src/Ouroboros/Network/Diffusion.hs
@@ -23,10 +23,12 @@ module Ouroboros.Network.Diffusion
   where
 
 import           Data.Functor (void)
+import           Control.Exception (IOException)
+
+import           Network.Socket (Socket)
 
 import           Ouroboros.Network.NodeToNode
                   ( RemoteAddress
-                  , Socket
                   , NodeToNodeVersionData
                   , NodeToNodeVersion
                   )
@@ -53,6 +55,7 @@ data ExtraTracers (p2p :: P2P) where
     :: P2P.TracersExtra
            RemoteAddress  NodeToNodeVersion   NodeToNodeVersionData
            LocalAddress   NodeToClientVersion NodeToClientVersionData
+           IOException IO
     -> ExtraTracers 'P2P
 
   NonP2PTracers
@@ -62,26 +65,26 @@ data ExtraTracers (p2p :: P2P) where
 
 -- | Diffusion arguments which depend on p2p mode.
 --
-data ExtraArguments (p2p :: P2P) where
+data ExtraArguments (p2p :: P2P) m where
   P2PArguments
-    :: P2P.ArgumentsExtra
-    -> ExtraArguments 'P2P
+    :: P2P.ArgumentsExtra m
+    -> ExtraArguments 'P2P m
 
   NonP2PArguments
     :: NonP2P.ArgumentsExtra
-    -> ExtraArguments 'NonP2P
+    -> ExtraArguments 'NonP2P m
 
 
 -- | Application data which depend on p2p mode.
 --
-data ExtraApplications (p2p :: P2P) ntnAddr where
+data ExtraApplications (p2p :: P2P) ntnAddr m where
   P2PApplications
-    :: P2P.ApplicationsExtra ntnAddr
-    -> ExtraApplications 'P2P ntnAddr
+    :: P2P.ApplicationsExtra ntnAddr m
+    -> ExtraApplications 'P2P ntnAddr m
 
   NonP2PApplications
     :: NonP2P.ApplicationsExtra
-    -> ExtraApplications 'NonP2P ntnAddr
+    -> ExtraApplications 'NonP2P ntnAddr m
 
 
 -- | Run data diffusion in either 'P2P' or 'NonP2P' mode.
@@ -90,15 +93,17 @@ run :: forall (p2p :: P2P).
        Tracers
          RemoteAddress NodeToNodeVersion
          LocalAddress  NodeToClientVersion
+         IO
     -> ExtraTracers p2p
     -> Arguments
          Socket      RemoteAddress
          LocalSocket LocalAddress
-    -> ExtraArguments p2p
+    -> ExtraArguments p2p IO
     -> Applications
          RemoteAddress  NodeToNodeVersion   NodeToNodeVersionData
          LocalAddress   NodeToClientVersion NodeToClientVersionData
-    -> ExtraApplications p2p RemoteAddress
+         IO
+    -> ExtraApplications p2p RemoteAddress IO
     -> IO ()
 run tracers (P2PTracers tracersExtra)
             args (P2PArguments argsExtra)

ouroboros-network/src/Ouroboros/Network/PeerSelection/Simple.hs
@@ -14,10 +14,11 @@ module Ouroboros.Network.PeerSelection.Simple
 
 import           Data.Foldable (toList)
 import           Control.Monad.Class.MonadAsync
+import           Control.Monad.Class.MonadThrow
 import           Control.Monad.Class.MonadTime
+import           Control.Monad.Class.MonadTimer
 import           Control.Monad.Class.MonadSTM.Strict
 import           Control.Tracer (Tracer)
-import           Control.Exception (IOException)
 
 import           Data.Map (Map)
 import           Data.Set (Set)
@@ -33,28 +34,36 @@ import           Ouroboros.Network.PeerSelection.RootPeersDNS
 
 
 withPeerSelectionActions
-  :: forall peeraddr peerconn a.
-     Ord peeraddr
-  => Tracer IO (TraceLocalRootPeers peeraddr IOException)
-  -> Tracer IO TracePublicRootPeers
+  :: forall peeraddr peerconn resolver exception m a.
+     ( MonadAsync m
+     , MonadDelay m
+     , MonadThrow m
+     , Ord peeraddr
+     , Exception exception
+     , Eq (Async m Void)
+     )
+  => Tracer m (TraceLocalRootPeers peeraddr exception)
+  -> Tracer m TracePublicRootPeers
   -> (IP -> Socket.PortNumber -> peeraddr)
-  -> STM IO PeerSelectionTargets
-  -> STM IO [(Int, Map RelayAccessPoint PeerAdvertise)]
+  -> DNSActions resolver exception m
+  -> STM m PeerSelectionTargets
+  -> STM m [(Int, Map RelayAccessPoint PeerAdvertise)]
   -- ^ local root peers
-  -> STM IO [RelayAccessPoint]
+  -> STM m [RelayAccessPoint]
   -- ^ public root peers
-  -> PeerStateActions peeraddr peerconn IO
-  -> (NumberOfPeers -> IO (Maybe (Set peeraddr, DiffTime)))
-  -> (Maybe (Async IO Void)
-      -> PeerSelectionActions peeraddr peerconn IO
-      -> IO a)
+  -> PeerStateActions peeraddr peerconn m
+  -> (NumberOfPeers -> m (Maybe (Set peeraddr, DiffTime)))
+  -> (Maybe (Async m Void)
+      -> PeerSelectionActions peeraddr peerconn m
+      -> m a)
   -- ^ continuation, recieves a handle to the local roots peer provider thread
   -- (only if local root peers where non-empty).
-  -> IO a
+  -> m a
 withPeerSelectionActions
   localRootTracer
   publicRootTracer
   toPeerAddr
+  dnsActions
   readTargets
   readLocalRootPeers
   readPublicRootPeers
@@ -65,7 +74,7 @@ withPeerSelectionActions
     let peerSelectionActions = PeerSelectionActions {
             readPeerSelectionTargets = readTargets,
             readLocalRootPeers = toList <$> readTVar localRootsVar,
-            requestPublicRootPeers = requestPublicRootPeers ioDNSActions,
+            requestPublicRootPeers = requestPublicRootPeers,
             requestPeerGossip = \_ -> pure [],
             peerStateActions
           }
@@ -74,7 +83,7 @@ withPeerSelectionActions
         localRootTracer
         toPeerAddr
         DNS.defaultResolvConf
-        ioDNSActions
+        dnsActions
         readLocalRootPeers
         localRootsVar)
       (\thread -> k (Just thread) peerSelectionActions)
@@ -82,20 +91,18 @@ withPeerSelectionActions
     -- We first try to get poublic root peers from the ledger, but if it fails
     -- (for example because the node hasn't synced far enough) we fall back
     -- to using the manually configured bootstrap root peers.
-    requestPublicRootPeers :: DNSActions DNS.Resolver IOException IO
-                           -> Int -> IO (Set peeraddr, DiffTime)
-    requestPublicRootPeers dnsActions n = do
+    requestPublicRootPeers :: Int -> m (Set peeraddr, DiffTime)
+    requestPublicRootPeers n = do
       peers_m <- getLedgerPeers (NumberOfPeers $ fromIntegral n)
       case peers_m of
-           Nothing    -> requestConfiguredRootPeers dnsActions n
+           Nothing    -> requestConfiguredRootPeers n
            Just peers -> return peers
 
     -- For each call we re-initialise the dns library which forces reading
     -- `/etc/resolv.conf`:
     -- https://github.com/input-output-hk/cardano-node/issues/731
-    requestConfiguredRootPeers :: DNSActions DNS.Resolver IOException IO
-                               -> Int -> IO (Set peeraddr, DiffTime)
-    requestConfiguredRootPeers dnsActions n =
+    requestConfiguredRootPeers :: Int -> m (Set peeraddr, DiffTime)
+    requestConfiguredRootPeers n =
       publicRootPeersProvider publicRootTracer
                               toPeerAddr
                               DNS.defaultResolvConf