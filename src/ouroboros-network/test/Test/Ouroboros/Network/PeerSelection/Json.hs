IntersectMBO/ouroboros-network

peer-selection: test json instances
* use `.&&.`
* wire the tests at the top level; this makes it easier to find out how
  one can run them.

ouroboros-network/test/Test/Ouroboros/Network/PeerSelection/Json.hs
@@ -12,27 +12,29 @@ import           Test.Tasty.QuickCheck (testProperty)
 
 tests :: TestTree
 tests =
-  testGroup "PeerSelection Types JSON instances"
-  [ testProperty "DomainAddress roundtrip" prop_roundtrip_DomainAddress_JSON
-  , testProperty "RelayAddress roundtrip"  prop_roundtrip_RelayAddress_JSON
-  , testProperty "PeerAdvertise roundtrip" prop_roundtrip_PeerAdvertise_JSON
+  testGroup "Ouroboros.Network.PeerSelection"
+  [ testGroup "JSON"
+    [ testProperty "DomainAddress roundtrip" prop_roundtrip_DomainAddress_JSON
+    , testProperty "RelayAddress roundtrip"  prop_roundtrip_RelayAddress_JSON
+    , testProperty "PeerAdvertise roundtrip" prop_roundtrip_PeerAdvertise_JSON
+    ]
   ]
 
 prop_roundtrip_DomainAddress_JSON :: DomainAddress -> Property
 prop_roundtrip_DomainAddress_JSON da =
     decode (encode da) === Just da
-    .&.
+    .&&.
     fromJSON (toJSON da) === pure da
 
 prop_roundtrip_RelayAddress_JSON :: RelayAddress -> Property
 prop_roundtrip_RelayAddress_JSON ra =
     decode (encode ra) === Just ra
-    .&.
+    .&&.
     fromJSON (toJSON ra) === pure ra
 
 prop_roundtrip_PeerAdvertise_JSON :: PeerAdvertise -> Property
 prop_roundtrip_PeerAdvertise_JSON pa =
     decode (encode pa) === Just pa
-    .&.
+    .&&.
     fromJSON (toJSON pa) === pure pa
 

IntersectMBO/cardano-world

src/ouroboros-network/test/Test/Ouroboros/Network/PeerSelection/Json.hs
@@ -13,27 +13,29 @@ import           Test.Tasty.QuickCheck (testProperty)
 
 tests :: TestTree
 tests =
-  testGroup "Ouroboros.Network.PeerSelection.Json"
-  [ testProperty "DomainAccessPoint roundtrip" prop_roundtrip_DomainAccessPoint_JSON
-  , testProperty "RelayAccessPoint roundtrip"  prop_roundtrip_RelayAccessPoint_JSON
-  , testProperty "PeerAdvertise roundtrip"     prop_roundtrip_PeerAdvertise_JSON
+  testGroup "Ouroboros.Network.PeerSelection"
+  [ testGroup "JSON"
+    [ testProperty "DomainAccessPoint roundtrip" prop_roundtrip_DomainAccessPoint_JSON
+    , testProperty "RelayAccessPoint roundtrip"  prop_roundtrip_RelayAccessPoint_JSON
+    , testProperty "PeerAdvertise roundtrip"     prop_roundtrip_PeerAdvertise_JSON
+    ]
   ]
 
 prop_roundtrip_DomainAccessPoint_JSON :: DomainAccessPoint -> Property
 prop_roundtrip_DomainAccessPoint_JSON da =
     decode (encode da) === Just da
-    .&.
+    .&&.
     fromJSON (toJSON da) === pure da
 
 prop_roundtrip_RelayAccessPoint_JSON :: RelayAccessPoint -> Property
 prop_roundtrip_RelayAccessPoint_JSON ra =
     decode (encode ra) === Just ra
-    .&.
+    .&&.
     fromJSON (toJSON ra) === pure ra
 
 prop_roundtrip_PeerAdvertise_JSON :: PeerAdvertise -> Property
 prop_roundtrip_PeerAdvertise_JSON pa =
     decode (encode pa) === Just pa
-    .&.
+    .&&.
     fromJSON (toJSON pa) === pure pa
 