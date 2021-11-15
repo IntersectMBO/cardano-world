ilyar/ouroboros-network

connection-manager: simulation test

ouroboros-network-framework/test/Main.hs
@@ -4,8 +4,9 @@ import           Test.Tasty
 
 import qualified Test.Network.TypedProtocol.PingPong.Codec as PingPong
 import qualified Test.Network.TypedProtocol.ReqResp.Codec as ReqResp
-import qualified Test.Ouroboros.Network.Server2 as Server2
+import qualified Test.Ouroboros.Network.ConnectionManager as ConnectionManager
 import qualified Test.Ouroboros.Network.Driver as Driver
+import qualified Test.Ouroboros.Network.Server2 as Server2
 import qualified Test.Ouroboros.Network.Socket as Socket
 import qualified Test.Ouroboros.Network.Subscription as Subscription
 import qualified Test.Ouroboros.Network.RateLimiting as RateLimiting
@@ -16,7 +17,8 @@ main = defaultMain tests
 tests :: TestTree
 tests =
   testGroup "ouroboros-network-framework"
-  [ PingPong.tests
+  [ ConnectionManager.tests
+  , PingPong.tests
   , ReqResp.tests
   , Driver.tests
   , Server2.tests

IntersectMBO/cardano-world

src/ouroboros-network-framework/test/Main.hs
@@ -2,8 +2,9 @@ module Main (main) where
 
 import           Test.Tasty
 
-import qualified Test.Ouroboros.Network.Server2 as Server2
+import qualified Test.Ouroboros.Network.ConnectionManager as ConnectionManager
 import qualified Test.Ouroboros.Network.Driver as Driver
+import qualified Test.Ouroboros.Network.Server2 as Server2
 import qualified Test.Ouroboros.Network.Socket as Socket
 import qualified Test.Ouroboros.Network.Subscription as Subscription
 import qualified Test.Ouroboros.Network.RateLimiting as RateLimiting
@@ -14,7 +15,8 @@ main = defaultMain tests
 tests :: TestTree
 tests =
   testGroup "ouroboros-network-framework"
-  [ Driver.tests
+  [ ConnectionManager.tests
+  , Driver.tests
   , Server2.tests
   , Socket.tests
   , Subscription.tests