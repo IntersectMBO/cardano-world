locallycompact/ouroboros-network

Increment NodeToClient version and integrate LocalTxMonitor as client mini-protocol.

ouroboros-network/src/Ouroboros/Network/NodeToClient.hs
@@ -38,6 +38,7 @@ module Ouroboros.Network.NodeToClient (
   , chainSyncPeerNull
   , localStateQueryPeerNull
   , localTxSubmissionPeerNull
+  , localTxMonitorPeerNull
 
   -- * Re-exported network interface
   , IOManager (..)
@@ -110,6 +111,8 @@ import qualified Ouroboros.Network.Protocol.ChainSync.Type   as ChainSync
 import           Ouroboros.Network.Protocol.ChainSync.Client as ChainSync
 import qualified Ouroboros.Network.Protocol.LocalTxSubmission.Type   as LocalTxSubmission
 import           Ouroboros.Network.Protocol.LocalTxSubmission.Client as LocalTxSubmission
+import qualified Ouroboros.Network.Protocol.LocalTxMonitor.Type   as LocalTxMonitor
+import           Ouroboros.Network.Protocol.LocalTxMonitor.Client as LocalTxMonitor
 import qualified Ouroboros.Network.Protocol.LocalStateQuery.Type   as LocalStateQuery
 import           Ouroboros.Network.Protocol.LocalStateQuery.Client as LocalStateQuery
 import           Ouroboros.Network.Protocol.Handshake.Codec
@@ -170,7 +173,8 @@ nodeToClientProtocols protocols version =
         NodeToClientProtocols {
             localChainSyncProtocol,
             localTxSubmissionProtocol,
-            localStateQueryProtocol
+            localStateQueryProtocol,
+            localTxMonitorProtocol
           } ->
           [ localChainSyncMiniProtocol localChainSyncProtocol
           , localTxSubmissionMiniProtocol localTxSubmissionProtocol
@@ -179,7 +183,11 @@ nodeToClientProtocols protocols version =
           | case version of
               NodeToClientV_1 -> False
               _               -> True
+          ] <>
+          [ localTxMonitorMiniProtocol localTxMonitorProtocol
+          | version >= NodeToClientV_11
           ]
+
   where
     localChainSyncMiniProtocol localChainSyncProtocol = MiniProtocol {
         miniProtocolNum    = MiniProtocolNum 5,
@@ -196,6 +204,11 @@ nodeToClientProtocols protocols version =
         miniProtocolLimits = maximumMiniProtocolLimits,
         miniProtocolRun    = localStateQueryProtocol
       }
+    localTxMonitorMiniProtocol localTxMonitorProtocol = MiniProtocol {
+        miniProtocolNum    = MiniProtocolNum 9,
+        miniProtocolLimits = maximumMiniProtocolLimits,
+        miniProtocolRun    = localTxMonitorProtocol
+    }
 
 maximumMiniProtocolLimits :: MiniProtocolLimits
 maximumMiniProtocolLimits =
@@ -453,6 +466,14 @@ localTxSubmissionPeerNull
 localTxSubmissionPeerNull =
     LocalTxSubmission.localTxSubmissionClientPeer
       (LocalTxSubmission.LocalTxSubmissionClient untilTheCowsComeHome)
+
+localTxMonitorPeerNull
+    :: forall (txid :: Type) (tx :: Type) (slot :: Type) m a. MonadTimer m
+    => Peer (LocalTxMonitor.LocalTxMonitor txid tx slot)
+            AsClient LocalTxMonitor.StIdle m a
+localTxMonitorPeerNull =
+    LocalTxMonitor.localTxMonitorClientPeer
+      (LocalTxMonitor.LocalTxMonitorClient untilTheCowsComeHome)
 
 -- ;)
 untilTheCowsComeHome :: MonadTimer m => m a

IntersectMBO/cardano-world

src/ouroboros-network/src/Ouroboros/Network/NodeToClient.hs
@@ -33,6 +33,8 @@ module Ouroboros.Network.NodeToClient
   , chainSyncPeerNull
   , localStateQueryPeerNull
   , localTxSubmissionPeerNull
+  , localTxMonitorPeerNull
+
     -- * Re-exported network interface
   , IOManager (..)
   , AssociateWithIOCP
@@ -105,6 +107,8 @@ import           Ouroboros.Network.Protocol.Handshake.Type
 import           Ouroboros.Network.Protocol.Handshake.Version hiding (Accept)
 import           Ouroboros.Network.Protocol.LocalStateQuery.Client as LocalStateQuery
 import qualified Ouroboros.Network.Protocol.LocalStateQuery.Type as LocalStateQuery
+import           Ouroboros.Network.Protocol.LocalTxMonitor.Client as LocalTxMonitor
+import qualified Ouroboros.Network.Protocol.LocalTxMonitor.Type   as LocalTxMonitor
 import           Ouroboros.Network.Protocol.LocalTxSubmission.Client as LocalTxSubmission
 import qualified Ouroboros.Network.Protocol.LocalTxSubmission.Type as LocalTxSubmission
 import           Ouroboros.Network.Snocket
@@ -164,7 +168,8 @@ nodeToClientProtocols protocols version =
         NodeToClientProtocols {
             localChainSyncProtocol,
             localTxSubmissionProtocol,
-            localStateQueryProtocol
+            localStateQueryProtocol,
+            localTxMonitorProtocol
           } ->
           [ localChainSyncMiniProtocol localChainSyncProtocol
           , localTxSubmissionMiniProtocol localTxSubmissionProtocol
@@ -173,7 +178,11 @@ nodeToClientProtocols protocols version =
           | case version of
               NodeToClientV_1 -> False
               _               -> True
+          ] <>
+          [ localTxMonitorMiniProtocol localTxMonitorProtocol
+          | version >= NodeToClientV_12
           ]
+
   where
     localChainSyncMiniProtocol localChainSyncProtocol = MiniProtocol {
         miniProtocolNum    = MiniProtocolNum 5,
@@ -190,6 +199,11 @@ nodeToClientProtocols protocols version =
         miniProtocolLimits = maximumMiniProtocolLimits,
         miniProtocolRun    = localStateQueryProtocol
       }
+    localTxMonitorMiniProtocol localTxMonitorProtocol = MiniProtocol {
+        miniProtocolNum    = MiniProtocolNum 9,
+        miniProtocolLimits = maximumMiniProtocolLimits,
+        miniProtocolRun    = localTxMonitorProtocol
+    }
 
 maximumMiniProtocolLimits :: MiniProtocolLimits
 maximumMiniProtocolLimits =
@@ -447,6 +461,14 @@ localTxSubmissionPeerNull
 localTxSubmissionPeerNull =
     LocalTxSubmission.localTxSubmissionClientPeer
       (LocalTxSubmission.LocalTxSubmissionClient untilTheCowsComeHome)
+
+localTxMonitorPeerNull
+    :: forall (txid :: Type) (tx :: Type) (slot :: Type) m a. MonadTimer m
+    => Peer (LocalTxMonitor.LocalTxMonitor txid tx slot)
+            AsClient LocalTxMonitor.StIdle m a
+localTxMonitorPeerNull =
+    LocalTxMonitor.localTxMonitorClientPeer
+      (LocalTxMonitor.LocalTxMonitorClient untilTheCowsComeHome)
 
 -- ;)
 untilTheCowsComeHome :: MonadTimer m => m a