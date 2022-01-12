IntersectMBO/cardano-node

cardano-node:  integrate trace-dispatcher

cardano-node/src/Cardano/Node/Protocol/Types.hs
@@ -5,12 +5,15 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE MonoLocalBinds #-}
 
 module Cardano.Node.Protocol.Types
   ( Protocol(..)
   , SomeConsensusProtocol(..)
+  , protocolName
   ) where
 
+import           Prelude (String)
 import           Cardano.Prelude
 
 import           Control.Monad.Fail (fail)
@@ -21,7 +24,8 @@ import qualified Cardano.Api.Protocol.Types as Cardano
 
 import           Cardano.Node.Orphans ()
 import           Cardano.Node.Queries (HasKESInfo, HasKESMetricsData)
-import           Cardano.Tracing.Constraints (TraceConstraints)
+import           Cardano.Node.TraceConstraints (TraceConstraints)
+
 
 data Protocol = ByronProtocol
               | ShelleyProtocol
@@ -47,8 +51,6 @@ instance FromJSON Protocol where
       _ -> fail $ "Parsing of Protocol failed. "
                 <> show str <> " is not a valid protocol"
 
-
-
 data SomeConsensusProtocol where
 
      SomeConsensusProtocol :: forall blk. ( Cardano.Protocol IO blk
@@ -59,3 +61,10 @@ data SomeConsensusProtocol where
                            => Cardano.BlockType blk
                            -> Cardano.ProtocolInfoArgs IO blk
                            -> SomeConsensusProtocol
+
+-- | A human readable name for the protocol
+--
+protocolName :: Protocol -> String
+protocolName ByronProtocol   = "Byron"
+protocolName ShelleyProtocol = "Shelley"
+protocolName CardanoProtocol = "Byron; Shelley"

IntersectMBO/cardano-world

cardano-node/src/Cardano/Node/Protocol/Types.hs
@@ -11,9 +11,9 @@ module Cardano.Node.Protocol.Types
   , SomeConsensusProtocol(..)
   ) where
 
-import           Cardano.Prelude
+import           Prelude
+import           Cardano.Prelude (Generic, NFData)
 
-import           Control.Monad.Fail (fail)
 import           Data.Aeson
 import           NoThunks.Class (NoThunks)
 
@@ -21,12 +21,18 @@ import qualified Cardano.Api.Protocol.Types as Cardano
 
 import           Cardano.Node.Orphans ()
 import           Cardano.Node.Queries (HasKESInfo, HasKESMetricsData)
-import           Cardano.Tracing.Constraints (TraceConstraints)
+import           Cardano.Node.TraceConstraints (TraceConstraints)
+
 
 data Protocol = ByronProtocol
               | ShelleyProtocol
               | CardanoProtocol
-  deriving (Eq, Show, Generic)
+  deriving (Eq, Generic)
+
+instance Show Protocol where
+  show ByronProtocol = "Byron"
+  show ShelleyProtocol = "Shelley"
+  show CardanoProtocol = "Byron; Shelley"
 
 deriving instance NFData Protocol
 deriving instance NoThunks Protocol
@@ -46,8 +52,6 @@ instance FromJSON Protocol where
 
       _ -> fail $ "Parsing of Protocol failed. "
                 <> show str <> " is not a valid protocol"
-
-
 
 data SomeConsensusProtocol where
 