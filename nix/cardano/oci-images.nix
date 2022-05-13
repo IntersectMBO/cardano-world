060bf9b17d097622b6462ad075a0430b4c5c5e12
David Arnold <david.arnold@iohk.io>
2022-05-13 09:42:33 -0500

    imp: tremendous image push speed optimization

--- nix/cardano/oci-images.nix
+++ nix/cardano/oci-images.nix
@@ -9,7 +9,10 @@ in {
   "cardano-node" = n2c.buildImage {
     name = "docker.infra.aws.iohkdev.io/cardano-node";
     maxLayers = 25;
-    layers = [(n2c.buildLayer {deps = [packages.cardano-node];})];
+    layers = [
+      (n2c.buildLayer {deps = entrypoints.cardano-node.runtimeInputs;})
+      (n2c.buildLayer {deps = [packages.cardano-node];})
+    ];
     contents = [nixpkgs.bashInteractive nixpkgs.iana-etc]; # healthChecks.cardano-node-network-sync];
     config.Cmd = [
       "${entrypoints.cardano-node}/bin/entrypoint"
@@ -18,6 +21,10 @@ in {
   "cardano-wallet" = n2c.buildImage {
     name = "docker.infra.aws.iohkdev.io/cardano-wallet";
     maxLayers = 25;
+    layers = [
+      (n2c.buildLayer {deps = entrypoints.cardano-wallet.runtimeInputs;})
+      (n2c.buildLayer {deps = [packages.cardano-wallet];})
+    ];
     contents = [nixpkgs.bashInteractive nixpkgs.iana-etc]; # healthChecks.cardano-wallet-network-sync];
     config.Cmd = [
       "${entrypoints.cardano-wallet}/bin/entrypoint"
@@ -26,6 +33,10 @@ in {
   "cardano-db-sync" = n2c.buildImage {
     name = "docker.infra.aws.iohkdev.io/cardano-db-sync";
     maxLayers = 25;
+    layers = [
+      (n2c.buildLayer {deps = entrypoints.cardano-db-sync.runtimeInputs;})
+      (n2c.buildLayer {deps = [packages.cardano-db-sync];})
+    ];
     contents = [nixpkgs.bashInteractive nixpkgs.iana-etc]; # healthChecks.cardano-db-sync-network-sync];
     config.Cmd = [
       "${entrypoints.cardano-db-sync}/bin/entrypoint"
@@ -34,6 +45,10 @@ in {
   "cardano-submit-api" = n2c.buildImage {
     name = "docker.infra.aws.iohkdev.io/cardano-submit-api";
     maxLayers = 25;
+    layers = [
+      (n2c.buildLayer {deps = entrypoints.cardano-submit-api.runtimeInputs;})
+      (n2c.buildLayer {deps = [packages.cardano-submit-api];})
+    ];
     contents = [nixpkgs.bashInteractive nixpkgs.iana-etc];
     config.Cmd = [
       "${entrypoints.cardano-submit-api}/bin/entrypoint"
