489810e2c54aa7ffd6205373b63f9f87a10104b9
David Arnold <david.arnold@iohk.io>
2022-05-12 22:12:59 -0500

    tmp: disable advanced healthcheck

--- nix/cardano/nomadJob/srv-node.nix
+++ nix/cardano/nomadJob/srv-node.nix
@@ -11,20 +11,20 @@
       timeout = "2s";
       type = "tcp";
     }
-    {
-      address_mode = "host";
-      args = [];
-      # FIXME: switch back to fully qualified invocation
-      # after: https://github.com/nlewo/nix2container/issues/15
-      # command = "${healthChecks.cardano-node-network-sync}/bin/healthcheck";
-      command = "/bin/healthcheck";
-      interval = "30s";
-      # on_update = "ignore_warnings";
-      # check_restart.ignore_warnings = true;
-      task = "node";
-      timeout = "10s";
-      type = "script";
-    }
+    # {
+    #   address_mode = "host";
+    #   args = [];
+    #   # FIXME: switch back to fully qualified invocation
+    #   # after: https://github.com/nlewo/nix2container/issues/15
+    #   # command = "${healthChecks.cardano-node-network-sync}/bin/healthcheck";
+    #   command = "/bin/healthcheck";
+    #   interval = "30s";
+    #   # on_update = "ignore_warnings";
+    #   # check_restart.ignore_warnings = true;
+    #   task = "node";
+    #   timeout = "10s";
+    #   type = "script";
+    # }
   ];
   name = "${namespace}-node";
   port = "node";
