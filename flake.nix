8e28517a470300eb66b9d002585b07fb3ce5b415
Samuel Leathers <samuel.leathers@iohk.io>
2022-06-08 11:12:56 -0400

    cardano-db-sync: bump to 13 rc2

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
     # --- Bride Heads ----------------------------------------------
     # TODO: remove when moved to monorepo
     cardano-node.url = "github:input-output-hk/cardano-node";
-    cardano-db-sync.url = "github:input-output-hk/cardano-db-sync/vasil-qa-deploy-0";
+    cardano-db-sync.url = "github:input-output-hk/cardano-db-sync/13.0.0-rc2";
     cardano-wallet.url = "github:input-output-hk/cardano-wallet";
     cardano-ogmios.url = "github:input-output-hk/cardano-ogmios/vasil";
     # --------------------------------------------------------------
