4890ca5aa358c83ecd1981ab0ec1fbb0f542f0b4
Samuel Leathers <samuel.leathers@iohk.io>
2022-06-10 14:24:06 -0400

    disable ogmios healthcheck

--- nix/cardano/nomadJob/srv-ogmios.nix
+++ nix/cardano/nomadJob/srv-ogmios.nix
@@ -4,19 +4,19 @@
 }: {
   address_mode = "auto";
   check = [
-    {
-      address_mode = "host";
-      args = ["health-check"];
-      # FIXME: switch back to fully qualified invocation
-      # after: https://github.com/nlewo/nix2container/issues/15
-      # command = "${healthChecks.cardano-wallet-network-sync}/bin/healthcheck";
-      command = "/bin/ogmios";
-      interval = "30s";
-      # on_update = "ignore_warnings";
-      # check_restart.ignore_warnings = true;
-      timeout = "10s";
-      type = "script";
-    }
+    #{
+    #  address_mode = "host";
+    #  args = ["health-check"];
+    #  # FIXME: switch back to fully qualified invocation
+    #  # after: https://github.com/nlewo/nix2container/issues/15
+    #  # command = "${healthChecks.cardano-wallet-network-sync}/bin/healthcheck";
+    #  command = "/bin/ogmios";
+    #  interval = "30s";
+    #  # on_update = "ignore_warnings";
+    #  # check_restart.ignore_warnings = true;
+    #  timeout = "10s";
+    #  type = "script";
+    #}
   ];
   task = "ogmios";
   name = "${namespace}-ogmios";
