92b715fe6fc2cad46566ad820bffdd0e559efde7
Kranium Gikos Mendoza <kraniumgikos.mendoza@iohk.io>
2022-08-02 20:56:22 +1000

    reintroduce cardano-wallet from stable-wallet-impl branch

--- nix/cardano/nomadCharts/default.nix
+++ nix/cardano/nomadCharts/default.nix
@@ -6,4 +6,5 @@
   cardano-node = import ./cardano-node.nix {inherit inputs cell;};
   cardano-db-sync = import ./cardano-db-sync.nix {inherit inputs cell;};
   cardano-faucet = import ./cardano-faucet.nix {inherit inputs cell;};
+  cardano-wallet = import ./cardano-wallet.nix {inherit inputs cell;};
 }
