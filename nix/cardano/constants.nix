575daa57c0a13a267281063dacc2ed1531d4a58c
Joshua Gilman <joshuagilman@gmail.com>
2022-11-09 07:58:27 -0800

    feat: updates mainnet snapshot

--- nix/cardano/constants.nix
+++ nix/cardano/constants.nix
@@ -23,7 +23,7 @@
     # https://update-cardano-mainnet.iohk.io/cardano-db-sync/index.html#13/
     mainnet = {
       base_url = "https://update-cardano-mainnet.iohk.io/cardano-db-sync/13";
-      file_name = "db-sync-snapshot-schema-13-block-7960123-x86_64.tgz";
+      file_name = "db-sync-snapshot-schema-13-block-7981218-x86_64.tgz";
     };
   };
 }
