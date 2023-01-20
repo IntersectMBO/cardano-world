c39f059c06bdc65a071caa22acb98f46fb32e302
John Lotoski <john.lotoski@iohk.io>
2023-01-19 18:29:38 -0600

    imp: mods required for nixpkgs bump
    * Remove bitte-cells inputs follows to avoid changing bitte-cells
      container content hash tag unnecessarily

--- nix/cardano/oci-images.nix
+++ nix/cardano/oci-images.nix
@@ -27,7 +27,7 @@ in {
       (n2c.buildLayer {deps = entrypoints.cardano-node.runtimeInputs;})
       (n2c.buildLayer {deps = [packages.cardano-node];})
     ];
-    contents = [nixpkgs.bashInteractive nixpkgs.iana-etc packages.cardano-node packages.cardano-cli];
+    copyToRoot = [nixpkgs.bashInteractive nixpkgs.iana-etc packages.cardano-node packages.cardano-cli];
     config.Cmd = [
       "${entrypoints.cardano-node}/bin/entrypoint"
     ];
@@ -40,7 +40,7 @@ in {
       (n2c.buildLayer {deps = entrypoints.cardano-db-sync.runtimeInputs;})
       (n2c.buildLayer {deps = [packages.cardano-db-sync];})
     ];
-    contents = [nixpkgs.bashInteractive nixpkgs.iana-etc rootCACerts];
+    copyToRoot = [nixpkgs.bashInteractive nixpkgs.iana-etc rootCACerts];
     config.Cmd = [
       "${entrypoints.cardano-db-sync}/bin/entrypoint"
     ];
@@ -53,7 +53,7 @@ in {
       (n2c.buildLayer {deps = entrypoints.cardano-wallet.runtimeInputs;})
       (n2c.buildLayer {deps = [packages.cardano-wallet];})
     ];
-    contents = [nixpkgs.bashInteractive nixpkgs.iana-etc healthChecks.cardano-wallet-network-sync];
+    copyToRoot = [nixpkgs.bashInteractive nixpkgs.iana-etc healthChecks.cardano-wallet-network-sync];
     config.Cmd = [
       "${entrypoints.cardano-wallet}/bin/entrypoint"
     ];
@@ -66,7 +66,7 @@ in {
       (n2c.buildLayer {deps = entrypoints.cardano-submit-api.runtimeInputs;})
       (n2c.buildLayer {deps = [packages.cardano-submit-api];})
     ];
-    contents = [nixpkgs.bashInteractive nixpkgs.iana-etc healthChecks.cardano-submit-api-network-sync];
+    copyToRoot = [nixpkgs.bashInteractive nixpkgs.iana-etc healthChecks.cardano-submit-api-network-sync];
     config.Cmd = [
       "${entrypoints.cardano-submit-api}/bin/entrypoint"
     ];
@@ -79,7 +79,7 @@ in {
       (n2c.buildLayer {deps = entrypoints.ogmios.runtimeInputs;})
       (n2c.buildLayer {deps = [packages.ogmios];})
     ];
-    contents = [nixpkgs.bashInteractive];
+    copyToRoot = [nixpkgs.bashInteractive];
     config.Cmd = [
       "${entrypoints.ogmios}/bin/entrypoint"
     ];
@@ -91,7 +91,7 @@ in {
     layers = [
       (n2c.buildLayer {deps = [packages.cardano-new-faucet];})
     ];
-    contents = [nixpkgs.bashInteractive];
+    copyToRoot = [nixpkgs.bashInteractive];
     config.Cmd = [
       "${entrypoints.cardano-faucet}/bin/entrypoint"
     ];
