27af1257f2ce5ca6f80283fe6a63daf9b53dcbf8
Kranium Gikos Mendoza <kraniumgikos.mendoza@iohk.io>
2023-02-10 13:40:06 +1100

    cardano-db-sync: use v13.1 mainnet snapshot

--- nix/cardano/constants.nix
+++ nix/cardano/constants.nix
@@ -22,8 +22,8 @@
     };
     # https://update-cardano-mainnet.iohk.io/cardano-db-sync/index.html#13/
     mainnet = {
-      base_url = "https://update-cardano-mainnet.iohk.io/cardano-db-sync/13";
-      file_name = "db-sync-snapshot-schema-13-block-7981218-x86_64.tgz";
+      base_url = "https://update-cardano-mainnet.iohk.io/cardano-db-sync/13.1";
+      file_name = "db-sync-snapshot-schema-13.1-block-8377643-x86_64.tgz";
     };
   };
 }
