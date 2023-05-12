b1691d8cb644cce586fe5cb51744bbb670cdec66
John Lotoski <john.lotoski@iohk.io>
2023-05-12 18:02:58 -0500

    imp: transform mkConfigHtml output to md book file struct

--- nix/cardano/library.nix
+++ nix/cardano/library.nix
@@ -3,12 +3,37 @@
   cell,
 }: let
   inherit (inputs) nixpkgs iohk-nix;
+  inherit (nixpkgs) lib runCommand;
   inherit (cardanoLib) mkConfigHtml;
   cardanoLib = import "${iohk-nix}/cardano-lib/default.nix" {inherit (nixpkgs) lib writeText runCommand jq;};
 
 in rec {
-  generateStaticHTMLConfigs = environments: mkConfigHtml environments;
+  # Use the iohk-nix mkConfigHtml attr and transform the output to what mdbook expects
+  generateStaticHTMLConfigs = environments: let
+    cardano-deployment = mkConfigHtml environments;
+  in runCommand "cardano-html" {} ''
+    mkdir "$out"
+    cp "${cardano-deployment}/index.html" "$out/"
+    cp "${cardano-deployment}/rest-config.json" "$out/"
+    ENVS=(${nixpkgs.lib.escapeShellArgs (builtins.attrNames environments)})
+    for ENV in "''${ENVS[@]}"; do
+      # Migrate each env from a flat dir to an ENV subdir
+      mkdir -p "$out/config/$ENV"
+      for i in $(find ${cardano-deployment} -type f -name "$ENV-*" -printf "%f\n"); do
+        cp -v "${cardano-deployment}/$i" "$out/config/$ENV/''${i#"$ENV-"}"
+      done
 
+      # Adjust genesis file and config refs
+      sed -i "s|\"$ENV-|\"|g" "$out/config/$ENV/config.json"
+      sed -i "s|\"$ENV-|\"|g" "$out/config/$ENV/db-sync-config.json"
+
+      # Adjust index.html file refs
+      sed -i "s|$ENV-|config/$ENV/|g" "$out/index.html"
+    done
+
+  '';
+
+  # Copy the environment configs from iohk-nix into our jobs at runtime
   copyEnvsTemplate = environments: let
     envCfgs = generateStaticHTMLConfigs environments;
   in ''
