f3bb49dea8bee16254e43c5a76c95cdb3ec3f0ea
John Lotoski <john.lotoski@iohk.io>
2024-04-16 18:50:36 -0500

    imp: rm expired DNS from explorer cert, failing acme renewal

--- nix/metal/explorer/aux-config.nix
+++ nix/metal/explorer/aux-config.nix
@@ -14,7 +14,7 @@ in
 {
   mainnet = lib.recursiveUpdate default {
     explorerHostName = "explorer.cardano.org";
-    explorerAliases = ["explorer.mainnet.cardano.org" "explorer.cardano-mainnet.iohk.io"];
+    explorerAliases = ["explorer.cardano-mainnet.iohk.io"];
 
     # WG_PUBKEY: wg pubkey < <(sops -d nix/metal/encrypted/wg/$AWS_EXT_NODES_NODENAME-private)
     # Attr struct: {name = $AWS_EXT_NODES_NODENAME; publicKey = $WG_PUBKEY;}
